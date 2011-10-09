18433a3c023a1ef27a6ef72b6fbb24b86c9e3164
ali-ebrahim <ali-ebrahim@bdb16a40-7426-4920-b3c5-7baf3776c1aa>
2011-10-09 18:46:36 +0000

    phenotypePhasePlane (in the oven) now can also store shadow prices (requires store_glpk_y_values.patch)

--- cobra/oven/phenotypePhasePlane.py
+++ cobra/oven/phenotypePhasePlane.py
@@ -1,5 +1,15 @@
-from numpy import linspace, zeros, array, meshgrid
-from mayavi import mlab
+from numpy import linspace, zeros, array, meshgrid, abs
+# not all plotting libraries may be installed
+try:
+    from mayavi import mlab
+except ImportError:
+    mlab = None
+try:
+    from matplotlib import pyplot
+    from mpl_toolkits.mplot3d import axes3d
+except ImportError:
+    pyplot = None
+    axes3d = None
 
 
 class phenotypePhasePlaneData:
@@ -23,9 +33,25 @@ class phenotypePhasePlaneData:
         self.shadow_prices2 = zeros((reaction1_npoints, reaction2_npoints))
 
     def plot(self):
-        self.plot_mlab()
+        self.plot_matplotlib()
+        # self.plot_mayavi()
 
-    def plot_mlab(self):
+    def plot_matplotlib(self):
+        if pyplot is None:
+            raise (ImportError, "Error importing matplotlib 3D plotting")
+        figure = pyplot.figure()
+        xgrid, ygrid = meshgrid(self.reaction1_fluxes, self.reaction2_fluxes)
+        axes = figure.add_subplot(111, projection="3d")
+        axes.plot_wireframe(xgrid, ygrid, self.growth_rates, color="black")
+        axes.plot_surface(xgrid, ygrid, self.growth_rates, alpha=0.75)
+        axes.set_xlabel(self.reaction1_name)
+        axes.set_ylabel(self.reaction2_name)
+        axes.set_zlabel("Growth rates")
+        pyplot.show()
+
+    def plot_mayavi(self):
+        if mlab is None:
+            raise (ImportError, "Error importing mayavi 3D plotting")
         mlab.figure(bgcolor=(1, 1, 1))
         max = 10.0
         xmax = self.reaction1_fluxes.max()
@@ -69,10 +95,12 @@ def calculate_phenotype_phase_plane(model,
             reaction1_range_max, reaction2_range_max,
             reaction1_npoints, reaction2_npoints)
     # find the objects for the reactions and metabolites
-    reaction1 = model.reactions.get_by_id(data.reaction1_name)
-    reaction2 = model.reactions.get_by_id(data.reaction2_name)
-    metabolite1 = model.metabolites.get_by_id(data.reaction1_name[3:])
-    metabolite2 = model.metabolites.get_by_id(data.reaction2_name[3:])
+    reaction1 = model.reactions[model.reactions.index(data.reaction1_name)]
+    reaction2 = model.reactions[model.reactions.index(data.reaction2_name)]
+    metabolite1 = model.metabolites[model.metabolites.index(
+        data.reaction1_name[3:])]
+    metabolite2 = model.metabolites[model.metabolites.index(
+        data.reaction2_name[3:])]
     # create a hot_start to help make solving faster
     hot_start = None
     for i in range(data.reaction1_npoints):
@@ -83,10 +111,15 @@ def calculate_phenotype_phase_plane(model,
             reaction2.upper_bound = -1 * data.reaction1_fluxes[j] + tolerance
             hot_start = model.optimize(the_problem=hot_start)
             if model.solution.status == "optimal":
-                # TODO - figure out how to get shadow prices out
                 data.growth_rates[i, j] = model.solution.f
+                data.shadow_prices1[i, j] =
+                    abs(model.solution.y_dict[data.reaction1_name])
+                data.shadow_prices2[i, j] =
+                    abs(model.solution.y_dict[data.reaction2_name])
             else:
                 data.growth_rates[i, j] = 0
+                data.shadow_prices1[i, j] = 0
+                data.shadow_prices2[i, j] = 0
     return data
 if __name__ == "__main__":
     import cobra
@@ -94,5 +127,5 @@ if __name__ == "__main__":
     model = create_cobra_model_from_sbml_file(
         r"C:\Python27\Lib\site-packages\cobra\examples\files\salmonella.xml")
     data = calculate_phenotype_phase_plane(model, 'EX_glc__D_e', 'EX_o2_e',
-        reaction1_npoints=20, reaction2_npoints=20)
+        reaction1_npoints=10, reaction2_npoints=10)
     data.plot()
