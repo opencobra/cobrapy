75e24bc1730124367870e49d5568090f10cddc24
ali-ebrahim <ali-ebrahim@bdb16a40-7426-4920-b3c5-7baf3776c1aa>
2011-10-10 17:23:24 +0000

    Improved coloring for phenotypePhasePlane.py (in python oven)

--- cobra/oven/phenotypePhasePlane.py
+++ cobra/oven/phenotypePhasePlane.py
@@ -1,4 +1,4 @@
-from numpy import linspace, zeros, array, meshgrid, abs
+from numpy import linspace, zeros, array, meshgrid, abs, empty
 # not all plotting libraries may be installed
 try:
     from mayavi import mlab
@@ -31,19 +31,33 @@ class phenotypePhasePlaneData:
         self.growth_rates = zeros((reaction1_npoints, reaction2_npoints))
         self.shadow_prices1 = zeros((reaction1_npoints, reaction2_npoints))
         self.shadow_prices2 = zeros((reaction1_npoints, reaction2_npoints))
+        self.segments = []
+        self.phases = []
 
     def plot(self):
-        self.plot_matplotlib()
-        # self.plot_mayavi()
+        if mlab is not None:
+            self.plot_mayavi()
+        else:
+            self.plot_matplotlib()
 
     def plot_matplotlib(self):
         if pyplot is None:
             raise (ImportError, "Error importing matplotlib 3D plotting")
+        # pick colors
+        color_list = ('r', 'g', 'b', 'c', 'y', 'm', 'k')
+        colors = empty(self.growth_rates.shape, dtype=str)
+        n_segments = self.segments.max()
+        if n_segments > 0 and n_segments <= len(color_list):
+            for i in range(n_segments):
+                colors[data.segments == (i + 1)] = color_list[i]
+        else:
+            colors[:, :] = 'b'
         figure = pyplot.figure()
         xgrid, ygrid = meshgrid(self.reaction1_fluxes, self.reaction2_fluxes)
         axes = figure.add_subplot(111, projection="3d")
         axes.plot_wireframe(xgrid, ygrid, self.growth_rates, color="black")
-        axes.plot_surface(xgrid, ygrid, self.growth_rates, alpha=0.75)
+        axes.plot_surface(xgrid, ygrid, self.growth_rates, rstride=1,
+            cstride=1, facecolors=colors, linewidth=0, antialiased=False)
         axes.set_xlabel(self.reaction1_name)
         axes.set_ylabel(self.reaction2_name)
         axes.set_zlabel("Growth rates")
@@ -65,7 +79,8 @@ class phenotypePhasePlaneData:
         zscale = max / zmax
         mlab.surf(xgrid * xscale, ygrid * yscale, self.growth_rates * zscale,
             representation="wireframe", color=(0, 0, 0))
-        mlab.surf(xgrid * xscale, ygrid * yscale, self.growth_rates * zscale,
+        mlab.mesh(xgrid * xscale, ygrid * yscale, self.growth_rates * zscale,
+            scalars=self.shadow_prices1 + self.shadow_prices2, resolution=1,
             representation="surface", opacity=0.75)
         # draw axes
         mlab.outline(extent=(0, max, 0, max, 0, max))
@@ -73,6 +88,31 @@ class phenotypePhasePlaneData:
         mlab.xlabel(self.reaction1_name)
         mlab.ylabel(self.reaction2_name)
         mlab.zlabel("Growth rates")
+
+    def segment(self, threshold=0.005):
+        self.segments = zeros(self.growth_rates.size)
+        # each entry in phases will consist of the following tuple
+        # ((x, y), shadow_price1, shadow_price2)
+        self.phases = []
+        # initialize the area to be all False
+        covered_area = (self.growth_rates * 0 == 1)
+        # as long as part of the area has not been covered
+        i = 0
+        j = 0
+        segment_id = 0
+        while covered_area.min() == False:
+            segment_id += 1
+            self.segments[
+                (abs(self.shadow_prices1 - self.shadow_prices1[i, j])
+                    < threshold) *
+                (abs(self.shadow_prices2 - self.shadow_prices2[i, j])
+                    < threshold)] = segment_id
+            self.phases.append((
+                (self.reaction1_fluxes[i], self.reaction2_fluxes[j]),
+                self.shadow_prices1[i, j], self.shadow_prices2[i, j]))
+            covered_area += self.segments[-1]
+            i = covered_area.argmin() / covered_area.shape[0]
+            j = covered_area.argmin() % covered_area.shape[0]
 
 
 # TODO - parallelize
@@ -112,20 +152,23 @@ def calculate_phenotype_phase_plane(model,
             hot_start = model.optimize(the_problem=hot_start)
             if model.solution.status == "optimal":
                 data.growth_rates[i, j] = model.solution.f
-                data.shadow_prices1[i, j] =
+                data.shadow_prices1[i, j] = \
                     abs(model.solution.y_dict[data.reaction1_name])
-                data.shadow_prices2[i, j] =
+                data.shadow_prices2[i, j] = \
                     abs(model.solution.y_dict[data.reaction2_name])
             else:
                 data.growth_rates[i, j] = 0
                 data.shadow_prices1[i, j] = 0
                 data.shadow_prices2[i, j] = 0
     return data
+
+
 if __name__ == "__main__":
     import cobra
     from cobra.io.sbml import create_cobra_model_from_sbml_file
     model = create_cobra_model_from_sbml_file(
         r"C:\Python27\Lib\site-packages\cobra\examples\files\salmonella.xml")
     data = calculate_phenotype_phase_plane(model, 'EX_glc__D_e', 'EX_o2_e',
-        reaction1_npoints=10, reaction2_npoints=10)
+        reaction1_npoints=20, reaction2_npoints=20)
+    data.segment()
     data.plot()
