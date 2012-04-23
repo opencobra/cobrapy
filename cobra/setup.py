17c4df3ee05d4b8510a8a5c2f42e6b7ca26a724e
danielhyduke <danielhyduke@bdb16a40-7426-4920-b3c5-7baf3776c1aa>
2012-04-23 20:28:43 +0000

    updating setup

--- cobra/setup.py
+++ cobra/setup.py
@@ -2,30 +2,23 @@ from setuptools import setup, find_packages
 setup(
     name = "COBRApy",
     version = '0.01a.dev',
-    packages = ['collections',
-                'core',
-                'external',
-                'flux_analysis',
-                'general',
-                'io',
-                'manipulation',
-                'mlab',
-                'query',
-                'stats',
-                'tools',
-                'topology'],
-    scripts = [''],
-
-    install_requires = ['numpy>=2.0.0',
-                        'scipy>=0.10.0',
-                        'rpy2>=2.2.2'],
+    packages = find_packages(exclude=['internal', 'oven', 'db_tools', 'omics', 'general']),
+    #scripts = [''],
+    setup_requires = ['numpy>=2.0.0.dev',
+                        'scipy>=0.10.0.dev',
+                        'rpy2>=2.2.2',
+                        'libsbml=5.1.0_b0'],
+    install_requires = ['numpy>=2.0.0.dev',
+                        'scipy>=0.10.0.dev',
+                        'rpy2>=2.2.2',
+                        'libsbml=5.1.0_b0'],
     extras_require = {
         'parallel': ["pp>=1.6"],
         'matlab': ["mlabwrap>=1.1"]
         },
 
     package_data = {
-        '': ['*.txt', '*.html']},
+        '': ['*.txt', '*.html','gpl-3.0','README']},
 
     author = "Daniel Robert Hyduke",
     author_email = "danielhyduke@gmail.com",
