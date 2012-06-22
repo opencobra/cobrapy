78edbc74ba0a734e7b51c030985bdea2edb36a9e
danielhyduke <danielhyduke@bdb16a40-7426-4920-b3c5-7baf3776c1aa>
2012-06-22 23:51:42 +0000

    added in the installers for cobra pie 0.1.0.b1

--- cobra/__init__.py
+++ cobra/__init__.py
@@ -1,4 +1,5 @@
 import sys
+__version__ = '0.1.0.b1'
 if hasattr(sys, 'JYTHON_JAR'):
     raise Exception("Experimental modules of numpy/scipy for java that are" +\
     "not yet ready for prime time.")
