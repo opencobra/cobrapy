699ea3e4eacacf327efd847495bd3ed1b83da3d4
danielhyduke <danielhyduke@bdb16a40-7426-4920-b3c5-7baf3776c1aa>
2012-08-13 21:57:15 +0000

    updated cobra py setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ import ez_setup
 ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 from pdb import set_trace
+__version = '0.2.0'
 setup(
     name = "cobra",
-    version = '0.2.0',
+    version = __version,
     packages = find_packages(exclude=['cobra.internal', 'cobra.oven', 'cobra.db_tools',
-                                      'cobra.omics', 'cobra.general',]),
+                                      'cobra.omics', 'cobra.oven*']),
     #scripts = [''],
     #put in numpy, scipy, libsbml, and pyglpk
     setup_requires = [],
@@ -37,6 +38,28 @@ setup(
     license = "GPL V3.0",
     keywords = "metabolism biology linear programming optimization flux balance analysis fba",
     url = "http://opencobra.sourceforge.net",
-    test_suite = "cobra.test.suite"
+    test_suite = "cobra.test.suite",
+    long_description = "COnstraint-Based Reconstruction and Analysis (COBRA) methods are widely used for genome-scale modeling of metabolic networks in both prokaryotes and eukaryotes.  COBRApy is a constraint-based modeling package that is designed to accomodate the biological complexity of the next generation of COBRA models and provides access to commonly used COBRA methods, such as flux balance analysis, flux variability analysis, and gene deletion analyses.  Through the mlabwrap module it is possible to use COBRApy to call many additional COBRA methods present in the COBRA Toolbox for MATLAB.",
+    download_url = 'http://sourceforge.net/projects/opencobra/files/python/cobra/' + __version,
+    classifiers = ['Development Status :: 5 - Production/Stable',
+                   'Environment :: Console',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+                   'Operating System :: MacOS :: MacOS X',
+                   'Operating System :: Microsoft :: Windows :: Windows 7',
+                   'Operating System :: Microsoft :: Windows :: Windows Vista',
+                   'Operating System :: Microsoft :: Windows :: Windows XP',
+                   'Operating System :: POSIX',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python :: 2.5',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: Implementation :: CPython',
+                   'Programming Language :: Python :: Implementation :: Jython',
+                   'Topic :: Scientific/Engineering',
+                   'Topic :: Scientific/Engineering :: Bio-Informatics'
+                   ],
+    platforms = "Python >= 2.6 on GNU/Linux, Mac OS X >= 10.7, Microsoft Windows >= 7. \n Jython >= 2.5 on Java >= 1.6"
+
     )
     
