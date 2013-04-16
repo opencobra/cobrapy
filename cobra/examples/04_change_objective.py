5fbece5fa03e1f77315201c14677664304cde67c
Daniel Robert Hyduke <danielhyduke@gmail.com>
2013-04-15 18:05:38 -0700

    updated values to match simulations based on current medium

--- cobra/examples/04_change_objective.py
+++ cobra/examples/04_change_objective.py
@@ -9,12 +9,12 @@ with open(salmonella_pickle) as in_file:
 
 
 cobra_model.optimize()
-print 'solution for old objective (should be approximately 0.380):'
+print 'solution for old objective (should be approximately 0.32):'
 print cobra_model.solution.f
 
 my_objective = cobra_model.reactions.get_by_id('3OAS140')
 print 'Changing objective to %s'%my_objective.id
 cobra_model.optimize(new_objective=my_objective)
-print 'solution for %s (should be approximately 0.845):'%my_objective.id
+print 'solution for %s (should be approximately 2.5):'%my_objective.id
 print cobra_model.solution.f
 
