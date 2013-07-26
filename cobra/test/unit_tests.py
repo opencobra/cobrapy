bd010f775dab715785b5f45911ec328bc487efc8
Ali Ebrahim <aebrahim@ucsd.edu>
2013-07-25 18:00:57 -0700

    include tests for deleting reactions

--- cobra/test/unit_tests.py
+++ cobra/test/unit_tests.py
@@ -6,13 +6,11 @@ from tempfile import gettempdir
 from os import unlink
 from copy import deepcopy
 from os.path import join
-try:  #skipIf is not in python 2.6 / 2.5
+try:  #skipIf is not in python 2.6 / 2.5, so use unittest2
     from unittest import skipIf
 except:
-    try:  # should we make unittest2 an absolute requirement and use skipIf
-        from unittest2 import skipIf
-    except:
-        skipIf = None
+    from unittest2 import skipIf
+
 
 # deal with absolute imports by adding the appropriate directory to the path
 if __name__ == "__main__":
@@ -70,11 +68,8 @@ class TestDictList(TestCase):
         self.assertEqual(self.list[8].id, "test9")
         self.assertEqual(len(self.list), 9)
 
+
     def testAdd(self):
-        from os import name as __name
-        if __name == 'java':
-            warn('\t\n**cobra.test.unit_tests.testAdd does not yet work with %s'%__name)
-            return
         obj_list = [Object("test%d" % (i)) for i in range(2, 10)]
         sum = self.list + obj_list
         self.assertEqual(self.list[0].id, "test1")
@@ -102,7 +97,6 @@ class TestCobraCore(CobraTestCase):
     """test core cobra functions"""
 
     def test_add_reaction(self):
-        """test adding and deleting reactions"""
         old_reaction_count = len(self.model.reactions)
         old_metabolite_count = len(self.model.metabolites)
         dummy_metabolite_1 = Metabolite("test_foo_1")
@@ -111,12 +105,22 @@ class TestCobraCore(CobraTestCase):
         dummy_reaction.add_metabolites({dummy_metabolite_1: -1,
                                         dummy_metabolite_2: 1})
         self.model.add_reaction(dummy_reaction)
-        self.assertEqual(self.model.reactions._object_dict[dummy_reaction.id],
+        self.assertEqual(self.model.reactions.get_by_id(dummy_reaction.id),
                          dummy_reaction)
         for x in [dummy_metabolite_1, dummy_metabolite_2]:
-            self.assertEqual(self.model.metabolites._object_dict[x.id],
-                             x)
-            
+            self.assertEqual(self.model.metabolites.get_by_id(x.id), x)
+        self.assertEqual(len(self.model.reactions), old_reaction_count + 1)
+        self.assertEqual(len(self.model.metabolites), old_metabolite_count + 2)
+
+    def test_delete_reaction(self):
+        old_reaction_count = len(self.model.reactions)
+        self.model.remove_reactions([self.model.reactions.get_by_id("PGI")])
+        self.assertEqual(len(self.model.reactions), old_reaction_count - 1)
+        with self.assertRaises(KeyError):
+            self.model.reactions.get_by_id("PGI")
+        # TODO - delete by id - will this be supported?
+        # TODO - delete orphan metabolites - will this be expected behavior?
+        
 
     def test_copy(self):
         """modifying copy should not modify the original"""
@@ -157,40 +161,44 @@ class TestCobraCore(CobraTestCase):
 
 class TestCobraIO(CobraTestCase):
     try:
-        from libsbml import SBMLReader as __SBMLReader
+        from libsbml import SBMLReader
         __test_sbml = True
     except:
         __test_sbml = False
-    if __test_sbml:
-        def test_sbml_read(self):
-            ## with catch_warnings(record=True) as w:
-            model = io.read_sbml_model(test_sbml_file)
-            self.assertEqual(len(model.reactions), len(self.model.reactions))
-            # make sure that an error is raised when given a nonexistent file
-            self.assertRaises(IOError, io.read_sbml_model,
-                              "fake_file_which_does_not_exist")
-        def test_sbml_write(self):
-            test_output_filename = join(gettempdir(), 'test_sbml_write.xml')
-            io.write_sbml_model(self.model, test_output_filename)
-            #cleanup the test file
-            unlink(test_output_filename)
     try:
-        from cobra.io import save_matlab_model
+        from scipy.io import loadmat
         __test_matlab = True
     except:
         __test_matlab = False
-    if __test_matlab:
-        def test_mat_read_write(self):
-            test_output_filename = join(gettempdir(), "test_mat_write.mat")
-            io.save_matlab_model(self.model, test_output_filename)
-            reread = io.load_matlab_model(test_output_filename)
-            self.assertEqual(len(self.model.reactions), len(reread.reactions))
-            self.assertEqual(len(self.model.metabolites), len(reread.metabolites))
-            for i in range(len(self.model.reactions)):
-                self.assertEqual(len(self.model.reactions[i]._metabolites), \
-                    len(reread.reactions[i]._metabolites))
-                self.assertEqual(self.model.reactions[i].id, reread.reactions[i].id)
-            unlink(test_output_filename)
+
+    @skipIf(not __test_sbml, "libsbml required")
+    def test_sbml_read(self):
+        ## with catch_warnings(record=True) as w:
+        model = io.read_sbml_model(test_sbml_file)
+        self.assertEqual(len(model.reactions), len(self.model.reactions))
+        # make sure that an error is raised when given a nonexistent file
+        self.assertRaises(IOError, io.read_sbml_model,
+                          "fake_file_which_does_not_exist")
+
+    @skipIf(not __test_sbml, "libsbml required")
+    def test_sbml_write(self):
+        test_output_filename = join(gettempdir(), 'test_sbml_write.xml')
+        io.write_sbml_model(self.model, test_output_filename)
+        #cleanup the test file
+        unlink(test_output_filename)
+    
+    @skipIf(not __test_matlab, "scipy.io.loadmat required")
+    def test_mat_read_write(self):
+        test_output_filename = join(gettempdir(), "test_mat_write.mat")
+        io.save_matlab_model(self.model, test_output_filename)
+        reread = io.load_matlab_model(test_output_filename)
+        self.assertEqual(len(self.model.reactions), len(reread.reactions))
+        self.assertEqual(len(self.model.metabolites), len(reread.metabolites))
+        for i in range(len(self.model.reactions)):
+            self.assertEqual(len(self.model.reactions[i]._metabolites), \
+                len(reread.reactions[i]._metabolites))
+            self.assertEqual(self.model.reactions[i].id, reread.reactions[i].id)
+        unlink(test_output_filename)
 
 # make a test suite to run all of the tests
 loader = TestLoader()
