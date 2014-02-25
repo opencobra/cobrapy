72dcaff6b46e560a24ef5f26a5f0a03b57eb7c52
Ali Ebrahim <aebrahim@ucsd.edu>
2014-02-24 20:00:14 -0800

    update_pickles should create a binary file
    The file is now opened with "wb" instead of simply "w" which prevents
    bugs when working between the mac/linux and windows.

--- cobra/test/data/update_pickles.py
+++ cobra/test/data/update_pickles.py
@@ -6,17 +6,23 @@ from cobra import Model
 from cobra.version import get_version
 from cobra.io import read_sbml_model, read_legacy_sbml
 from cobra.test import create_test_model
+
 model_names = ['salmonella', 'iJO1366', 'Yersinia_pestis_CO92_iPC815']
+
 for model_name in model_names:
+    # read in old pickle and model from sbml
     model_pickle = model_name + '.pickle'
     old_model = create_test_model(model_pickle)
     if model_name == "iJO1366":
         new_model = read_legacy_sbml(model_name + '.xml')
     else:
         new_model = read_sbml_model(model_name + '.xml')
-    [setattr(x, 'name', old_model.genes.get_by_id(x.id).name)
-     for x in new_model.genes]
+    # update any attributes from the old pickle
+    for x in new_model.genes:    
+        x.name = old_model.genes.get_by_id(x.id).name
     if hasattr(old_model, 'media_compositions'):
         new_model.media_compositions = old_model.media_compositions
     new_model._cobra_version = get_version()
-    dump(new_model, open(model_pickle, 'w'))
+    # write out new pickle
+    with open(model_pickle, 'wb') as outfile:
+        dump(new_model, outfile, protocol=2)
