815e44a47378774a062400dc72bbcb068010bfc4
Ali Ebrahim <aebrahim@ucsd.edu>
2014-03-18 17:53:40 -0700

    fix pickle updating script
    Does not build from old pickles anymore

--- cobra/test/data/update_pickles.py
+++ cobra/test/data/update_pickles.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 #This script regenerates pickles of cobra Models.  Should be
 #performed after updating core classes to prevent subtle bugs.
-from cPickle import load, dump
+try:
+    from cPickle import load, dump
+except:
+    from pickle import load, dump
+
+from os.path import isfile
+
 from cobra import Model
 from cobra.version import get_version
 from cobra.io import read_sbml_model, read_legacy_sbml
@@ -12,16 +18,19 @@ model_names = ['salmonella', 'iJO1366', 'Yersinia_pestis_CO92_iPC815']
 for model_name in model_names:
     # read in old pickle and model from sbml
     model_pickle = model_name + '.pickle'
-    old_model = create_test_model(model_pickle)
     if model_name == "iJO1366":
         new_model = read_legacy_sbml(model_name + '.xml')
     else:
         new_model = read_sbml_model(model_name + '.xml')
-    # update any attributes from the old pickle
-    for x in new_model.genes:    
-        x.name = old_model.genes.get_by_id(x.id).name
-    if hasattr(old_model, 'media_compositions'):
-        new_model.media_compositions = old_model.media_compositions
+    # update other attributes
+    if isfile(model_name + ".genes"):
+        with open(model_name + ".genes") as infile:
+            gene_names = load(infile)
+        for gene in new_model.genes:
+            gene.name = gene_names[gene.id]
+    if isfile(model_name + ".media"):
+        with open(model_name + ".media", "rb") as infile:
+            new_model.media_compositions = load(infile)
     new_model._cobra_version = get_version()
     # write out new pickle
     with open(model_pickle, 'wb') as outfile:
