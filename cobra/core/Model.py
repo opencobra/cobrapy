ee061fe743fbcc82fdd284d3a703a488939e7873
Ali Ebrahim <aebrahim@ucsd.edu>
2014-08-07 12:58:59 -0700

    Finish implementation of Model.repair
    This will:
     - recompute the relationship between genes and metabolites and their
       respecitve reactions.
     - Rebuild indexes for DictList
     - Point _model of genes, metabolites, and reactions to the model

--- cobra/core/Model.py
+++ cobra/core/Model.py
@@ -323,13 +323,29 @@ class Model(Object):
             except:
                 warn('%s not in %s'%(the_reaction, self))
 
-    def repair(self):
+    def repair(self, rebuild_index=True, rebuild_relationships=True):
         """Update all indexes and pointers in a model"""
-        # DictList indexes
-        self.reactions._generate_index()
-        self.metabolites._generate_index()
-        self.genes._generate_index()
-        return  # TODO update the pointers as well
+        if rebuild_index:  # DictList indexes
+            self.reactions._generate_index()
+            self.metabolites._generate_index()
+            self.genes._generate_index()
+        if rebuild_relationships:
+            for met in self.metabolites:
+                met._reaction.clear()
+            for gene in self.genes:
+                gene._reaction.clear()
+            for rxn in self.reactions:
+                for met in rxn._metabolites:
+                    met._reaction.add(rxn)
+                for gene in rxn._genes:
+                    gene._reaction.add(rxn)
+        # point _model to self
+        for l in (self.reactions, self.genes, self.metabolites):
+            for e in l:
+                e._model = self
+        if self.solution is None:
+            self.solution = Solution(None)
+        return
 
     def change_objective(self, objectives):
         """Change the objective in the cobrapy model.
