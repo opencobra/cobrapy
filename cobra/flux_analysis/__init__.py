01377918131c58b0604b94c40981811f044f098d
Ali Ebrahim <aebrahim@ucsd.edu>
2015-06-18 16:38:10 -0700

    graduate gapAnalysis to flux_analysis.gapfilling
    Formerly in the oven. Many bugs were also fixed.
    
    This adds python 3 support, pep8 compliance and relative imports.
    
    Documentation example also added.
    
    This fixes #163

--- cobra/flux_analysis/__init__.py
+++ cobra/flux_analysis/__init__.py
@@ -8,6 +8,7 @@ from .variability import flux_variability_analysis, find_blocked_reactions
 from .single_deletion import single_gene_deletion, single_reaction_deletion
 from .parsimonious import optimize_minimal_flux
 from .loopless import construct_loopless_model
+from .gapfilling import growMatch
 
 if numpy:
     from .double_deletion import double_reaction_deletion, double_gene_deletion
