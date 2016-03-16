e95418405aa00386ea6a458c2249bbdec51569b9
mvacher <michael.vhr@gmail.com>
2016-03-16 08:26:47 +0800

    Fixed bug in single_reaction_deletion_moma

--- cobra/flux_analysis/single_deletion.py
+++ cobra/flux_analysis/single_deletion.py
@@ -97,7 +97,7 @@ def single_reaction_deletion_moma(cobra_model, reaction_list, solver=None,
     if moma is None:
         raise RuntimeError("scipy required for moma")
     solver = solver_dict[solver if solver else get_solver_name(qp=True)]
-    moma_model, moma_obj = moma.create_euclidian_moma_model(cobra_model)
+    moma_model, moma_objective = moma.create_euclidian_moma_model(cobra_model)
 
     growth_rate_dict = {}
     status_dict = {}
