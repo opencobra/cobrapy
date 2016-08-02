c7356d3a2c5f2399a95b5ff0454c736d0782c0f3
Henning Redestig <henning.red@gmail.com>
2016-08-02 14:19:20 +0200

    failing twine test upload..

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 echo -e " ... running twine to deploy ... "
 echo -e "
+[distutils]
+index-servers=
+    test
+
 [pypirepository]
 repository = ${PYPI_REPOSITORY}
 " > ~/.pypirc
+
+cat ~/.pypirc
 
 pip install twine
 twine upload --skip-existing --username "${PYPI_USERNAME}" --password "${PYPI_PASSWORD}" \
