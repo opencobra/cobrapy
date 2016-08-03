b25fd66d9d8618f748a886179726b0e2be77c3d1
Henning Redestig <henning.red@gmail.com>
2016-08-03 14:07:33 +0200

    more trickery

--- config.sh
+++ config.sh
@@ -46,7 +46,8 @@ function run_tests_in_repo {
 	echo 'backend: Agg' >> $HOME/.config/matplotlib/matplotlibrc
 	echo -e "import cobra.test; cobra.test.test_all()" > run-tests.py
 	coverage run --source=cobra --rcfile .coveragerc run-tests.py
-	cat .coverage > ../.coverage
+	parentdir=`readlink -f ..`
+	cat .coverage | python -c "import re; import sys; import re; [sys.stdout.write(re.sub('/venv/lib/python.../site-packages/', '${parentdir}/', x)) for x in sys.stdin]" > ../.coverage
 }
 
 function run_tests {
