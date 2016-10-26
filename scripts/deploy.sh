01e99d429e462439c4e2fdef9acd75c77ceb47f1
Henning Redestig <henning.red@gmail.com>
2016-10-26 10:27:24 +0200

    Revert "Fix/broken wheels (#297)"
    This reverts commit 521c87b3ef8299ddef4df9f13f940ed79398fe1d.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,4 @@
 echo -e " ... running twine to deploy ... "
 pip install twine
 
-# only upload mac wheels for now, linux wheels are broken due to https://github.com/pypa/manylinux/issues/80
-twine upload --skip-existing --username "${PYPI_USERNAME}" --password "${PYPI_PASSWORD}" ${TRAVIS_BUILD_DIR}/wheelhouse/*macosx*.whl
+twine upload --skip-existing --username "${PYPI_USERNAME}" --password "${PYPI_PASSWORD}" ${TRAVIS_BUILD_DIR}/wheelhouse/*
