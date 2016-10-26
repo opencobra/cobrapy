a86ce62d752a8e821cb7fe22da09e3e767e2a800
Henning Redestig <henning.red@gmail.com>
2016-10-26 13:34:09 +0200

    port all tests to pytest
    - use fixtures
    - pep8
    - rename files to common conventions

---
+++ cobra/test/conftest.py
@@ -0,0 +1,43 @@
+from os.path import join
+from . import create_test_model, data_dir
+import pytest
+try:
+    from cPickle import load as _load
+except ImportError:
+    from pickle import load as _load
+import json
+
+
+@pytest.fixture(scope="session")
+def data_directory():
+    return data_dir
+
+
+@pytest.fixture(scope="function")
+def model():
+    return create_test_model("textbook")
+
+
+@pytest.fixture(scope="function")
+def array_model():
+    return create_test_model("textbook").to_array_based_model()
+
+
+@pytest.fixture(scope="function")
+def salmonella():
+    return create_test_model("salmonella")
+
+
+@pytest.fixture(scope="function")
+def solved_model(data_directory):
+    model = create_test_model("textbook")
+    with open(join(data_directory, "textbook_solution.pickle"),
+              "rb") as infile:
+        model.solution = _load(infile)
+    return model
+
+
+@pytest.fixture(scope="function")
+def fva_results(data_directory):
+    with open(join(data_directory, "textbook_fva.json"), "r") as infile:
+        return json.load(infile)
--- cobra/test/test_manipulation.py
+++ cobra/test/test_manipulation.py
@@ -1,115 +1,93 @@
-from unittest import TestCase, TestLoader, TextTestRunner
-
-import sys
-
-if __name__ == "__main__":
-    sys.path.insert(0, "../..")
-    from cobra.test import create_test_model, data_directory
-    from cobra.core import Metabolite, Model, Reaction
-    from cobra.manipulation import *
-    sys.path.pop(0)
-else:
-    from . import create_test_model, data_directory
-    from ..core import Metabolite, Model, Reaction
-    from ..manipulation import *
+from cobra.core import Metabolite, Model, Reaction
+from cobra.manipulation import *
 
 
-class TestManipulation(TestCase):
+class TestManipulation:
     """Test functions in cobra.manipulation"""
 
-    def test_canonical_form(self):
-        model = create_test_model("textbook")
+    def test_canonical_form(self, model):
         # add G constraint to test
         g_constr = Metabolite("SUCCt2_2__test_G_constraint")
         g_constr._constraint_sense = "G"
         g_constr._bound = 5.0
         model.reactions.get_by_id("SUCCt2_2").add_metabolites({g_constr: 1})
-        self.assertAlmostEqual(model.optimize("maximize").f, 0.855, places=3)
+        assert abs(model.optimize("maximize").f - 0.855) < 0.001
         # convert to canonical form
         model = canonical_form(model)
-        self.assertAlmostEqual(model.optimize("maximize").f, 0.855, places=3)
+        assert abs(model.optimize("maximize").f - 0.855) < 10 ** -3
 
-    def test_canonical_form_minimize(self):
-        model = create_test_model("textbook")
+    def test_canonical_form_minimize(self, model):
         # make a minimization problem
         model.reactions.get_by_id("Biomass_Ecoli_core").lower_bound = 0.5
         for reaction in model.reactions:
             reaction.objective_coefficient = reaction.id == "GAPD"
-        self.assertAlmostEqual(model.optimize("minimize").f, 6.27, places=3)
+        assert abs(model.optimize("minimize").f - 6.27) < 10 ** -3
         # convert to canonical form. Convert minimize to maximize
         model = canonical_form(model, objective_sense="minimize")
-        self.assertAlmostEqual(model.optimize("maximize").f, -6.27, places=3)
+        assert abs(model.optimize("maximize").f + 6.27) < 10 ** -3
         # lower bounds should now be <= constraints
-        self.assertEqual(
-            model.reactions.get_by_id("Biomass_Ecoli_core").lower_bound, 0.0)
+        assert model.reactions.get_by_id(
+            "Biomass_Ecoli_core").lower_bound == 0.0
 
-    def test_modify_reversible(self):
-        model1 = create_test_model("textbook")
+    def test_modify_reversible(self, model):
+        model1 = model.copy()
         model1.optimize()
-        model2 = create_test_model("textbook")
+        model2 = model.copy()
         convert_to_irreversible(model2)
         model2.optimize()
-        self.assertAlmostEqual(model1.solution.f, model2.solution.f, places=3)
+        assert abs(model1.solution.f - model2.solution.f) < 10 ** -3
         revert_to_reversible(model2)
         model2.optimize()
-        self.assertAlmostEqual(model1.solution.f, model2.solution.f, places=3)
-
+        assert abs(model1.solution.f - model2.solution.f) < 10 ** -3
         # Ensure revert_to_reversible is robust to solutions generated both
         # before and after reversibility conversion, or not solved at all.
-        model3 = create_test_model("textbook")
+        model3 = model.copy()
         model3.optimize()
         convert_to_irreversible(model3)
         revert_to_reversible(model3)
-        self.assertAlmostEqual(model1.solution.f, model3.solution.f, places=3)
-
+        assert abs(model1.solution.f - model3.solution.f) < 10 ** -3
         # test reaction where both bounds are negative
-        model4 = create_test_model("textbook")
+        model4 = model.copy()
         glc = model4.reactions.get_by_id("EX_glc__D_e")
         glc.upper_bound = -1
         convert_to_irreversible(model4)
         model4.optimize()
-        self.assertAlmostEqual(model1.solution.f, model4.solution.f, places=3)
+        assert abs(model1.solution.f - model4.solution.f) < 10 ** -3
         glc_rev = model4.reactions.get_by_id(glc.notes["reflection"])
-        self.assertEqual(glc_rev.lower_bound, 1)
-        self.assertEqual(glc.upper_bound, 0)
+        assert glc_rev.lower_bound == 1
+        assert glc.upper_bound == 0
         revert_to_reversible(model4)
-        self.assertEqual(glc.upper_bound, -1)
+        assert glc.upper_bound == -1
 
-    def test_escape_ids(self):
-        model = create_test_model('textbook')
+    def test_escape_ids(self, model):
         model.reactions.PGI.gene_reaction_rule = "a.b or c"
-        self.assertIn("a.b", model.genes)
+        assert "a.b" in model.genes
         escape_ID(model)
-        self.assertNotIn("a.b", model.genes)
+        assert "a.b" not in model.genes
 
-    def test_rename_gene(self):
-        model = create_test_model('textbook')
+    def test_rename_gene(self, model):
         original_name = model.genes.b1241.name
         rename_dict = {"b1241": "foo", "hello": "world",
                        "b2465": "b3919", "bar": "2935"}
         modify.rename_genes(model, rename_dict)
         for i in rename_dict:
-            self.assertNotIn(i, model.genes)
-        self.assertIn("foo", model.genes)
+            assert i not in model.genes
+        assert "foo" in model.genes
         # make sure the object name was preserved
-        self.assertEqual(model.genes.foo.name, original_name)
+        assert model.genes.foo.name == original_name
         # make sure the reactions are correct
-        self.assertEqual(len(model.genes.foo.reactions), 2)
-        self.assertEqual(model.reactions.ACALD.gene_reaction_rule,
-                         "b0351 or foo")
-        self.assertEqual(model.reactions.TPI.gene_reaction_rule, "b3919")
-        self.assertEqual(model.reactions.TPI.genes, {model.genes.b3919})
-        self.assertEqual(model.reactions.TKT1.gene_reaction_rule,
-                         "b2935 or b3919")
-        self.assertEqual(model.reactions.TKT1.genes,
-                         {model.genes.b2935, model.genes.b3919})
-        self.assertEqual(model.genes.b3919.reactions,
-                         {model.reactions.get_by_id(i)
-                          for i in ("TKT1", "TKT2", "TPI")})
+        assert len(model.genes.foo.reactions) == 2
+        assert model.reactions.ACALD.gene_reaction_rule == "b0351 or foo"
+        assert model.reactions.TPI.gene_reaction_rule == "b3919"
+        assert model.reactions.TPI.genes == {model.genes.b3919}
+        assert model.reactions.TKT1.gene_reaction_rule == "b2935 or b3919"
+        assert model.reactions.TKT1.genes == {
+            model.genes.b2935, model.genes.b3919}
+        assert model.genes.b3919.reactions == {model.reactions.get_by_id(i)
+                                               for i in
+                                               ("TKT1", "TKT2", "TPI")}
 
-    def test_gene_knockout_computation(self):
-        cobra_model = create_test_model()
-
+    def test_gene_knockout_computation(self, salmonella):
         def find_gene_knockout_reactions_fast(cobra_model, gene_list):
             compiled_rules = get_compiled_gene_reaction_rules(
                 cobra_model)
@@ -126,10 +104,10 @@ class TestManipulation(TestCase):
                                   for i in expected_reaction_ids}
             removed1 = set(find_gene_knockout_reactions(m, genes))
             removed2 = set(find_gene_knockout_reactions_fast(m, genes))
-            self.assertEqual(removed1, expected_reactions)
-            self.assertEqual(removed2, expected_reactions)
+            assert removed1 == expected_reactions
+            assert removed2 == expected_reactions
             delete_model_genes(m, gene_ids, cumulative_deletions=False)
-            self.assertEqual(get_removed(m), expected_reaction_ids)
+            assert get_removed(m) == expected_reaction_ids
             undelete_model_genes(m)
 
         gene_list = ['STM1067', 'STM0227']
@@ -137,24 +115,24 @@ class TestManipulation(TestCase):
                                '3HAD180', '3HAD100', '3HAD181', '3HAD120',
                                '3HAD60', '3HAD141', '3HAD161', 'T2DECAI',
                                '3HAD40'}
-        test_computation(cobra_model, gene_list, dependent_reactions)
-        test_computation(cobra_model, ['STM4221'], {'PGI'})
-        test_computation(cobra_model, ['STM1746.S'], {'4PEPTabcpp'})
+        test_computation(salmonella, gene_list, dependent_reactions)
+        test_computation(salmonella, ['STM4221'], {'PGI'})
+        test_computation(salmonella, ['STM1746.S'], {'4PEPTabcpp'})
         # test cumulative behavior
-        delete_model_genes(cobra_model, gene_list[:1])
-        delete_model_genes(cobra_model, gene_list[1:],
+        delete_model_genes(salmonella, gene_list[:1])
+        delete_model_genes(salmonella, gene_list[1:],
                            cumulative_deletions=True)
-        delete_model_genes(cobra_model, ["STM4221"],
+        delete_model_genes(salmonella, ["STM4221"],
                            cumulative_deletions=True)
         dependent_reactions.add('PGI')
-        self.assertEqual(get_removed(cobra_model), dependent_reactions)
+        assert get_removed(salmonella) == dependent_reactions
         # non-cumulative following cumulative
-        delete_model_genes(cobra_model, ["STM4221"],
+        delete_model_genes(salmonella, ["STM4221"],
                            cumulative_deletions=False)
-        self.assertEqual(get_removed(cobra_model), {'PGI'})
+        assert get_removed(salmonella) == {'PGI'}
         # make sure on reset that the bounds are correct
-        reset_bound = cobra_model.reactions.get_by_id("T2DECAI").upper_bound
-        self.assertEqual(reset_bound, 1000.)
+        reset_bound = salmonella.reactions.get_by_id("T2DECAI").upper_bound
+        assert reset_bound == 1000.
         # test computation when gene name is a subset of another
         test_model = Model()
         test_reaction_1 = Reaction("test1")
@@ -183,7 +161,7 @@ class TestManipulation(TestCase):
     def test_remove_genes(self):
         m = Model("test")
         m.add_reactions([Reaction("r" + str(i + 1)) for i in range(8)])
-        self.assertEqual(len(m.reactions), 8)
+        assert len(m.reactions) == 8
         rxns = m.reactions
         rxns.r1.gene_reaction_rule = "(a and b) or (c and a)"
         rxns.r2.gene_reaction_rule = "(a and b and d and e)"
@@ -193,34 +171,33 @@ class TestManipulation(TestCase):
         rxns.r6.gene_reaction_rule = "y"
         rxns.r7.gene_reaction_rule = "x or     z"
         rxns.r8.gene_reaction_rule = ""
-        self.assertIn("a", m.genes)
-        self.assertIn("x", m.genes)
+        assert "a" in m.genes
+        assert "x" in m.genes
         remove_genes(m, ["a"], remove_reactions=False)
-        self.assertNotIn("a", m.genes)
-        self.assertIn("x", m.genes)
-        self.assertEqual(rxns.r1.gene_reaction_rule, "")
-        self.assertEqual(rxns.r2.gene_reaction_rule, "")
-        self.assertEqual(rxns.r3.gene_reaction_rule, "b and c")
-        self.assertEqual(rxns.r4.gene_reaction_rule, "(f and b) or (b and c)")
-        self.assertEqual(rxns.r5.gene_reaction_rule, "x")
-        self.assertEqual(rxns.r6.gene_reaction_rule, "y")
-        self.assertEqual(rxns.r7.genes, {m.genes.x, m.genes.z})
-        self.assertEqual(rxns.r8.gene_reaction_rule, "")
+        assert "a" not in m.genes
+        assert "x" in m.genes
+        assert rxns.r1.gene_reaction_rule == ""
+        assert rxns.r2.gene_reaction_rule == ""
+        assert rxns.r3.gene_reaction_rule == "b and c"
+        assert rxns.r4.gene_reaction_rule == "(f and b) or (b and c)"
+        assert rxns.r5.gene_reaction_rule == "x"
+        assert rxns.r6.gene_reaction_rule == "y"
+        assert rxns.r7.genes == {m.genes.x, m.genes.z}
+        assert rxns.r8.gene_reaction_rule == ""
         remove_genes(m, ["x"], remove_reactions=True)
-        self.assertEqual(len(m.reactions), 7)
-        self.assertNotIn("r5", m.reactions)
-        self.assertNotIn("x", m.genes)
-        self.assertEqual(rxns.r1.gene_reaction_rule, "")
-        self.assertEqual(rxns.r2.gene_reaction_rule, "")
-        self.assertEqual(rxns.r3.gene_reaction_rule, "b and c")
-        self.assertEqual(rxns.r4.gene_reaction_rule, "(f and b) or (b and c)")
-        self.assertEqual(rxns.r6.gene_reaction_rule, "y")
-        self.assertEqual(rxns.r7.gene_reaction_rule, "z")
-        self.assertEqual(rxns.r7.genes, {m.genes.z})
-        self.assertEqual(rxns.r8.gene_reaction_rule, "")
+        assert len(m.reactions) == 7
+        assert "r5" not in m.reactions
+        assert "x" not in m.genes
+        assert rxns.r1.gene_reaction_rule == ""
+        assert rxns.r2.gene_reaction_rule == ""
+        assert rxns.r3.gene_reaction_rule == "b and c"
+        assert rxns.r4.gene_reaction_rule == "(f and b) or (b and c)"
+        assert rxns.r6.gene_reaction_rule == "y"
+        assert rxns.r7.gene_reaction_rule == "z"
+        assert rxns.r7.genes == {m.genes.z}
+        assert rxns.r8.gene_reaction_rule == ""
 
-    def test_SBO_annotation(self):
-        model = create_test_model("textbook")
+    def test_sbo_annotation(self, model):
         rxns = model.reactions
         rxns.EX_o2_e.annotation.clear()
         fake_DM = Reaction("DM_h_c")
@@ -230,46 +207,31 @@ class TestManipulation(TestCase):
         # an existing SBO annotation
         rxns.get_by_id("EX_h_e").annotation["SBO"] = "SBO:0000628"
         add_SBO(model)
-        self.assertEqual(rxns.EX_o2_e.annotation["SBO"], "SBO:0000627")
-        self.assertEqual(rxns.DM_h_c.annotation["SBO"], "SBO:0000628")
-        self.assertEqual(rxns.EX_h_e.annotation["SBO"], "SBO:0000628")
+        assert rxns.EX_o2_e.annotation["SBO"] == "SBO:0000627"
+        assert rxns.DM_h_c.annotation["SBO"] == "SBO:0000628"
+        assert rxns.EX_h_e.annotation["SBO"] == "SBO:0000628"
 
-    def test_validate_reaction_bounds(self):
-        model = create_test_model("textbook")
+    def test_validate_reaction_bounds(self, model):
         model.reactions[0].lower_bound = float("-inf")
         model.reactions[1].lower_bound = float("nan")
         model.reactions[0].upper_bound = float("inf")
         model.reactions[1].upper_bound = float("nan")
         errors = check_reaction_bounds(model)
-        self.assertEqual(len(errors), 4)
+        assert len(errors) == 4
 
-    def test_validate_formula_compartment(self):
-        model = create_test_model("textbook")
+    def test_validate_formula_compartment(self, model):
         model.metabolites[1].compartment = "fake"
         model.metabolites[1].formula = "(a*.bcde)"
         errors = check_metabolite_compartment_formula(model)
-        self.assertEqual(len(errors), 2)
+        assert len(errors) == 2
 
-    def test_validate_mass_balance(self):
-        model = create_test_model("textbook")
-        self.assertEqual(len(check_mass_balance(model)), 0)
+    def test_validate_mass_balance(self, model):
+        assert len(check_mass_balance(model)) == 0
         # if we remove the SBO term which marks the reaction as
         # mass balanced, then the reaction should be detected as
         # no longer mass balanced
         EX_rxn = model.reactions.query("EX")[0]
         EX_rxn.annotation.pop("SBO")
         balance = check_mass_balance(model)
-        self.assertEqual(len(balance), 1)
-        self.assertIn(EX_rxn, balance)
-
-
-# make a test suite to run all of the tests
-loader = TestLoader()
-suite = loader.loadTestsFromModule(sys.modules[__name__])
-
-
-def test_all():
-    TextTestRunner(verbosity=2).run(suite)
-
-if __name__ == "__main__":
-    test_all()
+        assert len(balance) == 1
+        assert EX_rxn in balance
