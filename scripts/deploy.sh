602621ae3d606c74b4621096f74b18647021b2d9
Henning Redestig <henning.red@gmail.com>
2016-11-08 12:37:17 +0100

    make deploy happen on devel to pypi test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 echo -e " ... running twine to deploy ... "
+echo -e "
+[distutils]
+index-servers=
+    pypirepository
+
+[pypirepository]
+repository: https://testpypi.python.org/pypi
+username: henred
+password: pippi_langstrump
+" > ~/.pypirc
+
 pip install twine
 
-twine upload --skip-existing --username "${PYPI_USERNAME}" --password "${PYPI_PASSWORD}" ${TRAVIS_BUILD_DIR}/wheelhouse/*
+if [[ "$TRAVIS_BRANCH" == "devel" ]]; then
+	twine upload --skip-existing ${TRAVIS_BUILD_DIR}/wheelhouse/* -r pypirepository
+elif [[ "$TRAVIS_BRANCH" == "master" ]]; then
+	twine upload --skip-existing --username "${PYPI_USERNAME}" --password "${PYPI_PASSWORD}" ${TRAVIS_BUILD_DIR}/wheelhouse/*
+fi
