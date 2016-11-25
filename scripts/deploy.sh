b364d4409b166ae28f82e030f46c4d15d7f45ac8
Henning Redestig <henning.red@gmail.com>
2016-11-25 18:33:48 +0100

    Fix/deploy problem (#320)
    * test upload not practical

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-echo -e "
-[distutils]
-index-servers=
-    pypirepository
 
-[pypirepository]
-repository: https://testpypi.python.org/pypi
-username: henred
-password: pippi_langstrump
-" > ~/.pypirc
+echo -e " starting deploy for branch ${TRAVIS_BRANCH} .."
 
-
-if [[ "$TRAVIS_BRANCH" == "devel" ]]; then
-	echo -e " ... running twine to TEST deploy ... "
-	pip install twine
-	twine upload --skip-existing ${TRAVIS_BUILD_DIR}/wheelhouse/* -r pypirepository
-elif [[ "$TRAVIS_BRANCH" == "master" ]]; then
-	echo -e " ... running twine to deploy proper ... "
-	pip install twine
-	twine upload --skip-existing --username "${PYPI_USERNAME}" --password "${PYPI_PASSWORD}" ${TRAVIS_BUILD_DIR}/wheelhouse/*
-fi
+pip install twine
+twine upload --skip-existing --username "${PYPI_USERNAME}" --password "${PYPI_PASSWORD}" ${TRAVIS_BUILD_DIR}/wheelhouse/*
