246eb7d5026abce78d47830f11e85d2e839f3745
Peter St. John <peterc.stjohn@gmail.com>
2017-01-17 14:06:44 -0800

    removing old medium code

--- cobra/manipulation/__init__.py
+++ cobra/manipulation/__init__.py
@@ -1,8 +1,7 @@
 from .delete import delete_model_genes, undelete_model_genes, remove_genes, \
     find_gene_knockout_reactions
-from .modify import initialize_growth_medium, convert_to_irreversible, \
-    revert_to_reversible, escape_ID, canonical_form, \
-    get_compiled_gene_reaction_rules
+from .modify import convert_to_irreversible, revert_to_reversible, escape_ID, \
+    canonical_form, get_compiled_gene_reaction_rules
 from .annotate import add_SBO
 from .validate import check_mass_balance, check_reaction_bounds, \
     check_metabolite_compartment_formula
