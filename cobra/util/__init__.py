9e654a8606191790c418a7cccd63f32d4300f040
Peter St. John <peterc.stjohn@gmail.com>
2017-01-17 16:36:31 -0800

    try/except numpy import

--- cobra/util/__init__.py
+++ cobra/util/__init__.py
@@ -1,4 +1,11 @@
 from cobra.util.util import *
 from cobra.util.context import *
 from cobra.util.solver import *
-from cobra.util.array import *
+
+try:
+    import numpy
+except ImportError:
+    numpy = None
+
+if numpy:
+    from cobra.util.array import *
