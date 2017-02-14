0ce42edc81e857e3932c8ec5d4ad884db6feb3c8
Henning Redestig <henning.red@gmail.com>
2017-02-14 13:35:13 +0100

    fix: remove defunct code
    - gapfilling: broken implementation, poor fit to the package and plenty of better alternatives
    - reporter metabolites: untested code destined for rewrite upon need
    - optknock: moves to cameo

--- cobra/topology/__init__.py
+++ cobra/topology/__init__.py
@@ -1,9 +1,2 @@
-from os import name as __name
-from sys import modules as __modules
-from warnings import warn
-if __name == 'java':
-    warn("%s is not yet supported on jython" % __modules[__name__])
+from cobra.topology.reporter_metabolites import identify_reporter_metabolites
 
-else:
-    from .reporter_metabolites import *
-del __name, __modules
