d594d7b914de72c3ec09c8166fdfdc6e127731c4
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2017-02-16 17:04:32 +0100

    intermediate step towards double deletion

--- cobra/flux_analysis/double_deletion.py
+++ cobra/flux_analysis/double_deletion.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import
-
-from itertools import chain, product
+import sys
+import multiprocessing
 from warnings import warn
+from itertools import (chain, product, combinations)
+from operator import attrgetter
+from collections import defaultdict
+from builtins import (map, dict)
+from future.utils import raise_
 
-import numpy
-from pandas import DataFrame
 from six import iteritems
+import numpy
 
-from cobra.flux_analysis.deletion_worker import (
-    CobraDeletionMockPool, CobraDeletionPool)
-from cobra.manipulation.delete import (
-    find_gene_knockout_reactions, get_compiled_gene_reaction_rules)
-from cobra.solvers import get_solver_name, solver_dict
+from ..solvers import get_solver_name, solver_dict
+from ..manipulation.delete import (find_gene_knockout_reactions,
+                                   get_compiled_gene_reaction_rules)
+from .deletion_worker import CobraDeletionPool, CobraDeletionMockPool
+import cobra.util.solver as sutil
 
 try:
     import scipy
@@ -22,6 +25,130 @@ except ImportError:
 else:
     from . import moma
 
+try:
+    from pandas import DataFrame
+except:
+    DataFrame = None
+
+
+def double_reaction_deletion(model,
+                             reaction_list1=None, reaction_list2=None,
+                             method="fba", num_cpu=None,
+                             solver=None, zero_cutoff=1e-12,
+                             **kwargs):
+    """
+    Sequentially delete pairs of reactions and record the objective value.
+
+    Parameters
+    ----------
+    cobra_model : cobra.Model
+        The metabolic model to perform double deletions in.
+
+    reaction_list1, reaction_list2 : list, optional
+        Lists of `cobra.Reaction`s (or their IDs) to be deleted. If not
+        provided, all reactions in the model sorted by ID will be used.
+        `reaction_list2` will use `reaction_list1` by default.
+
+    method: {"fba", "moma"}, optional
+        Procedure used to predict the growth rate.
+
+    solver: str, optional
+        This must be a QP-capable solver for MOMA. If left unspecified,
+        a suitable solver will be automatically chosen.
+
+    zero_cutoff: float, optional
+        When checking to see if a value is 0, this threshold is used.
+
+    Returns
+    -------
+    dict of dict
+        A sparse representation of all combinations of pairs of reaction
+        deletions without replacements.
+    """
+    try:
+        (legacy, solver) = sutil.choose_solver(model, solver, qp=(method == "moma"))
+    except sutil.SolverNotFound:
+        if method == "moma":
+            warn("Cannot use MOMA since no QP-capable solver was found. Falling"
+                 " back to FBA.")
+            (legacy, solver) = sutil.choose_solver(model, solver)
+        else:
+            (err_type, err_val, err_tb) = sys.exc_info()
+            raise_(err_type, err_val, err_tb)  # reraise for Python2&3
+
+    if legacy:
+        raise ValueError(
+            "Legacy solvers are not supported any longer. Please use one of"
+            " the optlang solver interfaces instead.")
+
+    if num_cpu is None:
+        try:
+            num_cpu = multiprocessing.num_cpu()
+        except NotImplementedError:
+            warn("Number of cores could not be detected - assuming 1.")
+            num_cpu = 1
+
+    if reaction_list1 is None:
+        reaction_list1 = sorted(model.reactions, key=attrgetter("id"))
+    else:
+        reaction_list1 = model.reactions.get_by_any(reaction_list1)
+
+    if reaction_list2 is None:
+        reaction_list2 = reaction_list1
+    else:
+        reaction_list2 = model.reactions.get_by_any(reaction_list2)
+
+    # Reactions with 0 flux in the current conditions have no impact on
+    # deletion. Determine those reactions now to speed up computation later.
+    solution = model.solve()
+    if solution.status == "optimal":
+        kwargs["growth"] = solution.objective_value
+        kwargs["zero_flux_reactions"] = \
+            {rxn for (rxn, flux) in solution.fluxes.items()
+             if abs(flux) < zero_cutoff}
+    else:
+        warn("Non-optimal ({0:s}) base solution.".format(solution.status))
+
+    args = list()
+    covered = set()
+    for (rxn_a, rxn_b) in combinations(reaction_list1, reaction_list2):
+        if rxn_a is rxn_b:
+            continue
+        id_a = rxn_a.id
+        id_b = rxn_b.id
+        if ((id_a, id_b) in covered) or ((id_b, id_a) in covered):
+            continue
+        args.append((model, id_a, id_b))
+        covered.add((id_a, id_b))
+
+    if num_cpu > 1:
+        # multiprocessing
+        chunk_size = len(covered) // (num_cpu * 2)
+        with multiprocessing.Pool(num_cpu) as pool:
+            results = pool.imap_unordered(_deletion_worker, args,
+                    chunksize=chunk_size)
+    else:
+        results = map(_deletion_worker, args)
+
+    double = defaultdict(dict)
+    for (id_a, id_b, growth) in results:
+        double[id_a][id_b] = growth
+
+    return dict(double)
+
+def _deletion_worker(args):
+    model = args[0]
+    rxn_ids = args[1:]
+    with model as mod:
+        for rxn_id in rxn_ids:
+            mod.reactions.get_by_id(rxn_id).knock_out()
+        solution = mod.solve()
+        growth = solution.objective_value
+        if solution.status != "optimal":
+            warn("Non-optimal solution in {0:s} knock-out.".format(
+                 str(rxn_ids)))
+            growth = None
+    return (growth,) + tuple(rxn_ids)
 
 # Utility functions
 def generate_matrix_indexes(ids1, ids2):
@@ -90,10 +217,11 @@ def format_results_frame(row_ids, column_ids, matrix, return_frame=False):
 
     Otherwise returns a dict of
     {"x": row_ids, "y": column_ids", "data": result_matrx}"""
-    if return_frame:
+    if return_frame and DataFrame:
         return DataFrame(data=matrix, index=row_ids, columns=column_ids)
-    else:
-        return {"x": row_ids, "y": column_ids, "data": matrix}
+    elif return_frame and not DataFrame:
+        warn("could not import pandas.DataFrame")
+    return {"x": row_ids, "y": column_ids, "data": matrix}
 
 
 def double_deletion(cobra_model, element_list_1=None, element_list_2=None,
@@ -114,95 +242,95 @@ def double_deletion(cobra_model, element_list_1=None, element_list_2=None,
         raise Exception("unknown element type")
 
 
-def double_reaction_deletion(cobra_model,
-                             reaction_list1=None, reaction_list2=None,
-                             method="fba", return_frame=False,
-                             solver=None, zero_cutoff=1e-12,
-                             **kwargs):
-    """sequentially knocks out pairs of reactions in a model
-
-    cobra_model : :class:`~cobra.core.Model.Model`
-        cobra model in which to perform deletions
-
-    reaction_list1 : [:class:`~cobra.core.Reaction.Reaction`:] (or their id's)
-        Reactions to be deleted. These will be the rows in the result.
-        If not provided, all reactions will be used.
-
-    reaction_list2 : [:class:`~cobra.core.Reaction`:] (or their id's)
-        Reactions to be deleted. These will be the rows in the result.
-        If not provided, reaction_list1 will be used.
-
-    method: "fba" or "moma"
-        Procedure used to predict the growth rate
-
-    solver: str for solver name
-        This must be a QP-capable solver for MOMA. If left unspecified,
-        a suitable solver will be automatically chosen.
-
-    zero_cutoff: float
-        When checking to see if a value is 0, this threshold is used.
-
-    return_frame: bool
-        If true, formats the results as a pandas.Dataframe. Otherwise
-        returns a dict of the form:
-        {"x": row_labels, "y": column_labels", "data": 2D matrix}
-    """
-    # handle arguments which need to be passed on
-    if solver is None:
-        solver = get_solver_name(qp=(method == "moma"))
-        kwargs["solver"] = solver
-    kwargs["zero_cutoff"] = zero_cutoff
-
-    # generate other arguments
-
-    # identifiers for reactions are their indexes
-    if reaction_list1 is None:
-        reaction_indexes1 = range(len(cobra_model.reactions))
-    else:
-        reaction_indexes1 = [cobra_model.reactions.index(r)
-                             for r in reaction_list1]
-    if reaction_list2 is None:
-        reaction_indexes2 = reaction_indexes1
-    else:
-        reaction_indexes2 = [cobra_model.reactions.index(r)
-                             for r in reaction_list2]
-    reaction_to_result = generate_matrix_indexes(reaction_indexes1,
-                                                 reaction_indexes2)
-
-    # Determine 0 flux reactions. If an optimal solution passes no flux
-    # through the deleted reactions, then we know removing them will
-    # not change the solution.
-    wt_solution = solver_dict[solver].solve(cobra_model)
-    if wt_solution.status == "optimal":
-        kwargs["wt_growth_rate"] = wt_solution.f
-        kwargs["no_flux_reaction_indexes"] = \
-            {i for i, v in enumerate(wt_solution.x) if abs(v) < zero_cutoff}
-    else:
-        warn("wild-type solution status is '%s'" % wt_solution.status)
-
-    # call the computing functions
-    if method == "fba":
-        results = _double_reaction_deletion_fba(
-            cobra_model, reaction_indexes1, reaction_indexes2,
-            reaction_to_result, **kwargs)
-    elif method == "moma":
-        results = _double_reaction_deletion_moma(
-            cobra_model, reaction_indexes1, reaction_indexes2,
-            reaction_to_result, **kwargs)
-    else:
-        raise ValueError("Unknown deletion method '%s'" % method)
-
-    # convert upper triangular matrix to full matrix
-    full_result = _format_upper_triangular_matrix(
-        [reaction_to_result[i] for i in reaction_indexes1],  # row indexes
-        [reaction_to_result[i] for i in reaction_indexes2],  # col indexes
-        results)
-
-    # format appropriately with labels
-    row_ids = [cobra_model.reactions[i].id for i in reaction_indexes1]
-    column_ids = [cobra_model.reactions[i].id for i in reaction_indexes2]
-    return format_results_frame(row_ids, column_ids,
-                                full_result, return_frame)
+#def double_reaction_deletion(cobra_model,
+#                             reaction_list1=None, reaction_list2=None,
+#                             method="fba", return_frame=False,
+#                             solver=None, zero_cutoff=1e-12,
+#                             **kwargs):
+#    """sequentially knocks out pairs of reactions in a model
+#
+#    cobra_model : :class:`~cobra.core.Model.Model`
+#        cobra model in which to perform deletions
+#
+#    reaction_list1 : [:class:`~cobra.core.Reaction.Reaction`:] (or their id's)
+#        Reactions to be deleted. These will be the rows in the result.
+#        If not provided, all reactions will be used.
+#
+#    reaction_list2 : [:class:`~cobra.core.Reaction`:] (or their id's)
+#        Reactions to be deleted. These will be the rows in the result.
+#        If not provided, reaction_list1 will be used.
+#
+#    method: "fba" or "moma"
+#        Procedure used to predict the growth rate
+#
+#    solver: str for solver name
+#        This must be a QP-capable solver for MOMA. If left unspecified,
+#        a suitable solver will be automatically chosen.
+#
+#    zero_cutoff: float
+#        When checking to see if a value is 0, this threshold is used.
+#
+#    return_frame: bool
+#        If true, formats the results as a pandas.Dataframe. Otherwise
+#        returns a dict of the form:
+#        {"x": row_labels, "y": column_labels", "data": 2D matrix}
+#    """
+#    # handle arguments which need to be passed on
+#    if solver is None:
+#        solver = get_solver_name(qp=(method == "moma"))
+#        kwargs["solver"] = solver
+#    kwargs["zero_cutoff"] = zero_cutoff
+#
+#    # generate other arguments
+#
+#    # identifiers for reactions are their indexes
+#    if reaction_list1 is None:
+#        reaction_indexes1 = range(len(cobra_model.reactions))
+#    else:
+#        reaction_indexes1 = [cobra_model.reactions.index(r)
+#                             for r in reaction_list1]
+#    if reaction_list2 is None:
+#        reaction_indexes2 = reaction_indexes1
+#    else:
+#        reaction_indexes2 = [cobra_model.reactions.index(r)
+#                             for r in reaction_list2]
+#    reaction_to_result = generate_matrix_indexes(reaction_indexes1,
+#                                                 reaction_indexes2)
+#
+#    # Determine 0 flux reactions. If an optimal solution passes no flux
+#    # through the deleted reactions, then we know removing them will
+#    # not change the solution.
+#    wt_solution = solver_dict[solver].solve(cobra_model)
+#    if wt_solution.status == "optimal":
+#        kwargs["wt_growth_rate"] = wt_solution.f
+#        kwargs["no_flux_reaction_indexes"] = \
+#            {i for i, v in enumerate(wt_solution.x) if abs(v) < zero_cutoff}
+#    else:
+#        warn("wild-type solution status is '%s'" % wt_solution.status)
+#
+#    # call the computing functions
+#    if method == "fba":
+#        results = _double_reaction_deletion_fba(
+#            cobra_model, reaction_indexes1, reaction_indexes2,
+#            reaction_to_result, **kwargs)
+#    elif method == "moma":
+#        results = _double_reaction_deletion_moma(
+#            cobra_model, reaction_indexes1, reaction_indexes2,
+#            reaction_to_result, **kwargs)
+#    else:
+#        raise ValueError("Unknown deletion method '%s'" % method)
+#
+#    # convert upper triangular matrix to full matrix
+#    full_result = _format_upper_triangular_matrix(
+#        [reaction_to_result[i] for i in reaction_indexes1],  # row indexes
+#        [reaction_to_result[i] for i in reaction_indexes2],  # col indexes
+#        results)
+#
+#    # format appropriately with labels
+#    row_ids = [cobra_model.reactions[i].id for i in reaction_indexes1]
+#    column_ids = [cobra_model.reactions[i].id for i in reaction_indexes2]
+#    return format_results_frame(row_ids, column_ids,
+#                                full_result, return_frame)
 
 
 def double_gene_deletion(cobra_model,
