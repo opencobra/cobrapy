98296eeaffcfff94edd97d3942f2768610fd44b4
Peter St. John <peterc.stjohn@gmail.com>
2017-03-02 10:39:55 -0700

    resetting repair function to devel-2s

--- cobra/core/model.py
+++ cobra/core/model.py
@@ -528,30 +528,6 @@ class Model(Object):
                 reaction._metabolites = {}
                 reaction._genes = set()
 
-    def repair(self, rebuild_index=True, rebuild_relationships=True):
-        """Update all indexes and pointers in a model"""
-        if rebuild_index:  # DictList indexes
-            self.reactions._generate_index()
-            self.metabolites._generate_index()
-            self.genes._generate_index()
-        if rebuild_relationships:
-            for met in self.metabolites:
-                met._reaction.clear()
-            for gene in self.genes:
-                gene._reaction.clear()
-            for rxn in self.reactions:
-                for met in rxn._metabolites:
-                    met._reaction.add(rxn)
-                for gene in rxn._genes:
-                    gene._reaction.add(rxn)
-        # point _model to self
-        for l in (self.reactions, self.genes, self.metabolites):
-            for e in l:
-                e._model = self
-        if self.solution is None:
-            self.solution = Solution(None)
-        return
-
     def _populate_solver(self, reaction_list, metabolite_list=None):
         """Populate attached solver with constraints and variables that
         model the provided reactions.
@@ -692,6 +668,37 @@ class Model(Object):
             #     'returned solution status is "%s"' % (
             #         self, solution.status))
         return solution
+
+    def repair(self, rebuild_index=True, rebuild_relationships=True):
+        """Update all indexes and pointers in a model
+        Parameters
+        ----------
+        rebuild_index : bool
+            rebuild the indices kept in reactions, metabolites and genes
+        rebuild_relationships : bool
+             reset all associations between genes, metabolites, model and
+             then re-add them.
+        """
+        if rebuild_index:  # DictList indexes
+            self.reactions._generate_index()
+            self.metabolites._generate_index()
+            self.genes._generate_index()
+        if rebuild_relationships:
+            for met in self.metabolites:
+                met._reaction.clear()
+            for gene in self.genes:
+                gene._reaction.clear()
+            for rxn in self.reactions:
+                for met in rxn._metabolites:
+                    met._reaction.add(rxn)
+                for gene in rxn._genes:
+                    gene._reaction.add(rxn)
+        # point _model to self
+        for l in (self.reactions, self.genes, self.metabolites):
+            for e in l:
+                e._model = self
+        if self.solution is None:
+            self.solution = Solution(None)
 
     @property
     def objective(self):
