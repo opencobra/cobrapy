7c59314d47bbc83405f6a1584a4171f1584f72d0
Henning Redestig <henning.red@gmail.com>
2017-03-27 20:24:39 +0200

    feat: expose _{to,from}_dict (#471)
    Useful functions outside cobrapy so give public names.

--- cobra/io/__init__.py
+++ cobra/io/__init__.py
@@ -2,7 +2,8 @@
 
 from __future__ import absolute_import
 
-from cobra.io.json import load_json_model, save_json_model, to_json
+from cobra.io.json import (load_json_model, save_json_model, to_json,
+                           model_from_dict, model_to_dict)
 from cobra.io.sbml3 import read_sbml_model, write_sbml_model
 from cobra.io.sbml import read_legacy_sbml
 from cobra.io.sbml import write_cobra_model_to_sbml_file as \
