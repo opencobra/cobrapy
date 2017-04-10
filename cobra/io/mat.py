f793e1282c91df719b355394210dbc1a9e7d2af1
Christian Diener <mail@cdiener.com>
2017-04-10 10:57:00 -0500

    Do not truncate single string names.

--- cobra/io/mat.py
+++ cobra/io/mat.py
@@ -9,6 +9,7 @@ from warnings import warn
 
 from numpy import object as np_object
 from numpy import array, inf, isinf
+from six import string_types
 
 from cobra.core import Metabolite, Model, Reaction
 from cobra.util import create_stoichiometric_matrix
@@ -187,7 +188,7 @@ def from_mat_struct(mat_struct, model_id=None, inf=inf):
         model.id = model_id
     elif "description" in m.dtype.names:
         description = m["description"][0, 0][0]
-        if len(description) > 1:
+        if not isinstance(description, string_types) and len(description) > 1:
             model.id = description[0]
             warn("Several IDs detected, only using the first.")
         else:
