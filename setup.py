a46abc5a69afed907fa9db25934bee180c7a3517
Henning Redestig <henning.red@gmail.com>
2017-04-21 13:38:05 +0200

    Bump version: 0.6.0a5 → 0.6.0a6

--- setup.py
+++ setup.py
@@ -144,7 +144,7 @@ except:
 
 setup(
     name="cobra",
-    version="0.6.0a5",
+    version="0.6.0a6",
     packages=find_packages(),
     setup_requires=setup_requirements,
     install_requires=["future", "swiglpk", "optlang>=1.1.4",
