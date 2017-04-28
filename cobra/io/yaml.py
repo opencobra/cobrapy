12bfa08aced58ad749351f218ce02517fe871710
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2017-04-28 10:22:11 +0200

    fix: properly initialize the dumper and loader

--- cobra/io/yaml.py
+++ cobra/io/yaml.py
@@ -2,13 +2,17 @@
 
 from __future__ import absolute_import
 
-from cobra.io.dict import model_to_dict, model_from_dict
-
-"""http://stackoverflow.com/a/21912744"""
+import io
 import yaml
 from collections import OrderedDict
 
+from six import string_types
+
+from cobra.io.dict import model_to_dict, model_from_dict
+
+
 def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
+    """http://stackoverflow.com/a/21912744"""
     class OrderedLoader(Loader):
         pass
 
@@ -17,35 +21,62 @@ def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
         return object_pairs_hook(loader.construct_pairs(node))
 
     OrderedLoader.add_constructor(
-        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-        construct_mapping)
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, construct_mapping)
     return yaml.load(stream, OrderedLoader)
 
 
 def ordered_dump(data, stream=None, Dumper=yaml.Dumper, **kwds):
+    """http://stackoverflow.com/a/21912744"""
     class OrderedDumper(Dumper):
         pass
 
     def _dict_representer(dumper, data):
         return dumper.represent_mapping(
-            yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-            data.items())
+            yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, data.items())
 
     OrderedDumper.add_representer(OrderedDict, _dict_representer)
     return yaml.dump(data, stream, OrderedDumper, **kwds)
 
 
-def to_yaml(model):
-    raise NotImplementedError()
+def to_yaml(model, ordered=True):
+    """
+    Return the model as a YAML string.
+    
+    Parameters
+    ----------
+    model : cobra.Model
+    ordered : bool, optional
+        Whether to create YAML representation with deterministic order (
+        default true).
+    """
+    return ordered_dump(model_to_dict(model, ordered), Dumper=yaml.SafeDumper)
 
 
 def from_yaml(document):
-    raise NotImplementedError()
+    """
+    Load cobra model from a YAML string
+    
+    Parameters
+    ----------
+    document : str
+        A string representation of a metabolic model in YAML format.
+    """
+    return model_from_dict(ordered_load(document, yaml.SafeLoader))
 
 
-def save_yaml_model(model, filename):
-    raise NotImplementedError()
+def save_yaml_model(model, filename, ordered=True):
+    document = to_yaml(model, ordered)
+    if isinstance(filename, string_types):
+        with io.open(filename, "w") as file_h:
+            file_h.write(document)
+    else:
+        filename.write(document)
 
 
 def load_yaml_model(filename):
-    raise NotImplementedError()
+    if isinstance(filename, string_types):
+        with io.open(filename, "r") as file_h:
+            document = from_yaml(file_h.read())
+    else:
+        document = from_yaml(filename.read())
+    return document
