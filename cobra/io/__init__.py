c6d373f55a754e751246a9aeec98189da778d0a8
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2017-05-01 14:53:00 +0200

    fix: make public all relevant functions

--- cobra/io/__init__.py
+++ cobra/io/__init__.py
@@ -3,8 +3,10 @@
 from __future__ import absolute_import
 
 from cobra.io.dict import (model_from_dict, model_to_dict)
-from cobra.io.json import (load_json_model, save_json_model, to_json)
-from cobra.io.yaml import (load_yaml_model, save_yaml_model, to_yaml)
+from cobra.io.json import (
+    to_json, from_json, load_json_model, save_json_model)
+from cobra.io.yaml import (
+    to_yaml, from_yaml, load_yaml_model, save_yaml_model)
 from cobra.io.sbml3 import read_sbml_model, write_sbml_model
 from cobra.io.sbml import read_legacy_sbml
 from cobra.io.sbml import write_cobra_model_to_sbml_file as \
