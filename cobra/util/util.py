6e18d6474361227f3f1c7e60bf68021eed609cc4
Midnighter <midnighter@posteo.net>
2017-05-02 11:12:11 +0200

    refactor: slim down `FrozenDict` interface

--- cobra/util/util.py
+++ cobra/util/util.py
@@ -5,48 +5,17 @@ from __future__ import absolute_import
 import sympy
 import logging
 import operator
-from collections import Mapping, OrderedDict
+from collections import Mapping
 
-from six.moves import reduce
-
-
-class FrozenDict(dict):
-    def __init__(self, iterable, **kwargs):
-        super(FrozenDict, self).__init__(iterable, **kwargs)
-
-    def popitem(self):
-        raise AttributeError("'Frozendict' object has no attribute 'popitem")
-
-    def pop(self, k, d=None):
-        raise AttributeError("'Frozendict' object has no attribute 'pop")
-
-    def __setitem__(self, key, value):
-        raise AttributeError(
-            "'Frozendict' object has no attribute '__setitem__")
-
-    def setdefault(self, k, d=None):
-        raise AttributeError(
-            "'Frozendict' object has no attribute 'setdefault")
-
-    def __delitem__(self, key):
-        raise AttributeError(
-            "'Frozendict' object has no attribute '__delitem__")
-
-    def __hash__(self):
-        return hash(tuple(sorted(self.items())))
-
-    def update(self, E=None, **F):
-        raise AttributeError("'Frozendict' object has no attribute 'update")
+from six import iteritems
 
 
-class FrozenOrderedDict(Mapping):
-    """
-    Frozen OrderedDict.
-    """
-
+class FrozenDict(Mapping):
     def __init__(self, *args, **kwargs):
-        self.__dict = OrderedDict(*args, **kwargs)
+        super(FrozenDict, self).__init__()
+        self.__dict = dict(*args, **kwargs)
         self.__hash = None
+        self.__items = None
 
     def __getitem__(self, item):
         return self.__dict[item]
@@ -58,19 +27,22 @@ class FrozenOrderedDict(Mapping):
         return len(self.__dict)
 
     def __hash__(self):
+        if self.__items is None:
+            self.__items = sorted(iteritems(self.__dict))
         if self.__hash is None:
-            self.__hash = reduce(operator.xor, map(hash, self.iteritems()), 0)
-
+            self.__hash = hash(self.__items)
         return self.__hash
 
     def __repr__(self):
-        return '{}({!r})'.format(self.__class__.__name__, self.items())
+        if self.__items is None:
+            self.__items = sorted(iteritems(self.__dict))
+        return '{}({!r})'.format(self.__class__.__name__, self.__items)
 
     def copy(self, *args, **kwargs):
         new_dict = self.__dict.copy()
 
         if args or kwargs:
-            new_dict.update(OrderedDict(*args, **kwargs))
+            new_dict.update(dict(*args, **kwargs))
 
         return self.__class__(new_dict)
 
