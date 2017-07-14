e593dd5f184013cb0b76b67901bb8de1765b9625
Henning Redestig <henning.red@gmail.com>
2017-07-14 10:11:34 +0200

    Bump version: 0.6.2 → 0.7.0

--- setup.py
+++ setup.py
@@ -144,7 +144,7 @@ except:
 
 setup(
     name="cobra",
-    version="0.6.2",
+    version="0.7.0",
     packages=find_packages(),
     setup_requires=setup_requirements,
     install_requires=["future", "swiglpk", "optlang>=1.1.5", "ruamel.yaml<0.15",
