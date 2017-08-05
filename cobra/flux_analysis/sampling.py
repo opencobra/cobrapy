7629980105bfd554f1f048a370c1ce383b1ee189
Christian Diener <ch.diener@gmail.com>
2017-08-05 04:09:22 -0500

    fix: [sampling] fix a reprojection bug (#564)
    In rare cases the reprojection will yield bounds violations. If that occurs along with an infeasible sampling center the sampler will fail. This PR now guarantees that sampler._reproject always returns a feasible point and will not attempt to reproject already feasible points either (like it did before).

--- cobra/flux_analysis/sampling.py
+++ cobra/flux_analysis/sampling.py
@@ -280,9 +280,40 @@ class HRSampler(object):
                                       self.warmup[0:self.n_warmup, ])
 
     def _reproject(self, p):
-        """Reproject a point into the feasibility region."""
+        """Reproject a point into the feasibility region.
+
+        This function is guaranteed to return a new feasible point. However,
+        no guarantees in terms of proximity to the original point can be made.
+
+        Parameters
+        ----------
+        p : numpy.array
+            The current sample point.
+
+        Returns
+        -------
+        numpy.array
+            A new feasible point. If `p` was feasible it wil return p.
+        """
         nulls = self.problem.nullspace
-        return nulls.dot(nulls.T.dot(p))
+        equalities = self.problem.equalities
+        # don't reproject if point is feasible
+        if np.allclose(equalities.dot(p), self.problem.b,
+                       rtol=0, atol=feasibility_tol):
+            new = p
+        else:
+            LOGGER.info("feasibility violated in sample"
+                        " %d, trying to reproject" % self.n_samples)
+            new = nulls.dot(nulls.T.dot(p))
+        # Projections may violate bounds
+        # set to random point in space in that case
+        if any(self._bounds_dist(new) < -bounds_tol):
+            LOGGER.info("reprojection failed in sample"
+                        " %d, using random point in space" % self.n_samples)
+            idx = np.random.randint(self.n_warmup,
+                                    size=min(2, int(np.sqrt(self.n_warmup))))
+            new = self.warmup[idx, :].mean(axis=0)
+        return new
 
     def _bounds_dist(self, p):
         """Get the lower and upper bound distances. Negative is bad."""
