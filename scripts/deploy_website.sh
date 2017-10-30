4c287ace26574092b4fef60df04e57805426eb1b
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2017-10-30 15:07:32 +0100

    fix: use token authentication

--- scripts/deploy_website.sh
+++ scripts/deploy_website.sh
@@ -6,6 +6,12 @@ target=/tmp/cobrapy-website
 
 git clone "https://github.com/opencobra/cobrapy-website.git" "${target}"
 cd "${target}"
+
+git config user.name "Deployment Bot"
+git config user.email "deploy@travis-ci.org"
+git remote rm origin
+git remote add origin "https://user:${GITHUB_TOKEN}@github.com/opencobra/cobrapy-website.git" &> /dev/null
+
 git checkout master
 python "${TRAVIS_BUILD_DIR}"/scripts/publish_release.py "${TRAVIS_BUILD_DIR}"/release-notes "${target}"/content/releases "${TRAVIS_TAG}"
 git add .
