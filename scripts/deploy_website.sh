233a999dc6f8921d86c9db5f9c68294b4a540f86
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2017-10-30 14:04:35 +0100

    fix: correct script name

--- scripts/deploy_website.sh
+++ scripts/deploy_website.sh
@@ -7,7 +7,7 @@ target=/tmp/cobrapy-website
 git clone "https://github.com/opencobra/cobrapy-website.git" "${target}"
 cd "${target}"
 git checkout master
-python "${TRAVIS_BUILD_DIR}"/scripts/publish_releases.py "${TRAVIS_BUILD_DIR}"/release-notes "${target}"/content/releases "${TRAVIS_TAG}"
+python "${TRAVIS_BUILD_DIR}"/scripts/publish_release.py "${TRAVIS_BUILD_DIR}"/release-notes "${target}"/content/releases "${TRAVIS_TAG}"
 git add .
 git commit -m "feat: publish ${TRAVIS_TAG} on $(date +'%F %T')"
 git push origin master
