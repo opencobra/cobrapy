1c37dc0e64666edd2370f1cf0916e65ac107fdd6
Moritz E. Beber <midnighter@posteo.net>
2018-05-14 18:08:26 +0200

    refactor: simplify dependency information (#704)

--- setup.py
+++ setup.py
@@ -47,7 +47,8 @@ setup(
         "numpy>=1.13",
         "pandas>=0.17.0",
         "optlang>=1.2.5",
-        "tabulate"
+        "tabulate",
+        "depinfo"
     ],
     tests_require=[
         "jsonschema > 2.5",
