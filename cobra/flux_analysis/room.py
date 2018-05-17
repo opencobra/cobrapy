1cb9413c42fe65620098cf14dd1cbf5fc8ded065
Synchon Mandal <synchonmandal@gmail.com>
2018-05-17 21:22:28 +0530

    added ROOM with tests

---
+++ cobra/flux_analysis/room.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+"""Contains functions to run Regulatory On/Off Minimization (ROOM)."""
+
+from __future__ import absolute_import
+
+from optlang.symbolics import Zero, NegativeOne, Add, Mul
+
+
+def add_room(model, solution=None, delta=0.03, epsilon=0.001, linear=False):
+    """Add constraints and objective for ROOM.
+
+    This function adds variables and constraints for applying regulatory
+    on/off minimization (ROOM) to the model.
+
+    Parameters
+    ----------
+    model : cobra.Model
+        The model to add ROOM constraints and objectve to.
+    solution : cobra.Solution
+        A previous solution to use as a reference.
+    delta: float
+        Relative range of tolerance; is additive in nature.
+        Default is 0.03.
+    epsilon: float
+        Absolute range of tolerance; is multiplicative in nature.
+        Default is 0.001.
+    linear : bool
+        Whether to use the linear ROOM formulation or not.
+        Default is False.
+
+    Notes
+    -----
+    The formulation used here is the same as stated in the original paper.
+    The mathematical expression is given below:
+
+    minimize \sum_{i=1}^m y^i
+    s.t. Sv = 0
+         v_min <= v <= v_max
+         v_j = 0
+         j ∈ A
+         for 1 <= i <= m
+         v_i - y_i(v_{max,i} - w_i^u) <= w_i^u      (1)
+         v_i - y_i(v_{min,i} - w_i^l) <= w_i^l      (2)
+         y_i ∈ {0,1}                                (3)
+         w_i^u = w_i + \delta|w_i| + \epsilon
+         w_i^l = w_i - \delta|w_i| - \epsilon
+
+    So, for the linear version of the ROOM , constraint (3) is relaxed to
+    0 <= y_i <= 1.
+
+    References
+    ----------
+    .. [1] Tomer Shlomi, Omer Berkman and Eytan Ruppin, "Regulatory on/off
+     minimization of metabolic flux changes after genetic perturbations",
+     PNAS 2005 102 (21) 7695-7700; doi:10.1073/pnas.0406346102
+
+    """
+
+    if 'room_old_objective' in model.solver.variables:
+        raise ValueError('model is already adjusted for ROOM')
+
+    # optimizes if no reference solution is provided
+    if solution is None:
+        solution = model.optimize()
+
+    problem = model.problem
+    variable = problem.Variable("room_old_objective",
+                                ub=solution.objective_value)
+    constraint = problem.Constraint(
+        model.solver.objective.expression - variable,
+        ub=0.0,
+        lb=0.0,
+        name="room_old_objective_constraint"
+    )
+    new_objective = Zero
+    vars_and_cons = [variable, constraint]
+
+    for rxn in model.reactions:
+        flux = solution.fluxes[rxn.id]
+
+        if linear is False:
+            y = problem.Variable("y_" + rxn.id, type="binary")
+        else:
+            y = problem.Variable("y_" + rxn.id, lb=0, ub=1)
+            delta = epsilon = Zero
+
+        # upper constraint
+        w_u = flux + (delta * abs(flux)) + epsilon
+        upper_const = problem.Constraint(
+            Add(rxn.flux_expression,
+                Mul(NegativeOne,
+                    y,
+                    Add(rxn.upper_bound,
+                        Mul(NegativeOne,
+                            w_u)))),
+            ub=w_u,
+            name="room_constraint_upper_" + rxn.id
+        )
+        # lower constraint
+        w_l = flux - (delta * abs(flux)) - epsilon
+        lower_const = problem.Constraint(
+            Add(rxn.flux_expression,
+                Mul(NegativeOne,
+                    y,
+                    Add(rxn.lower_bound,
+                        Mul(NegativeOne,
+                            w_l)))),
+            lb=w_l,
+            name="room_constraint_lower_" + rxn.id,
+        )
+        vars_and_cons.extend([y, upper_const, lower_const])
+        new_objective += y
+
+    model.add_cons_vars(vars_and_cons)
+    model.objective = problem.Objective(new_objective, direction="min")
--- cobra/test/test_flux_analysis.py
+++ cobra/test/test_flux_analysis.py
@@ -8,18 +8,20 @@ import warnings
 import math
 import pytest
 import numpy
+from pandas import Series
 from contextlib import contextmanager
 from optlang.interface import OPTIMAL, INFEASIBLE
 from six import StringIO, iteritems
 
 import cobra.util.solver as sutil
-from cobra.core import Metabolite, Model, Reaction
+from cobra.core import Metabolite, Model, Reaction, Solution
 from cobra.flux_analysis import *
 from cobra.flux_analysis.parsimonious import add_pfba
 from cobra.flux_analysis.sampling import ACHRSampler, OptGPSampler
 from cobra.flux_analysis.reaction import assess
 from cobra.exceptions import Infeasible
 from cobra.flux_analysis.moma import add_moma
+from cobra.flux_analysis.room import add_room
 
 # The scipy interface is currently unstable and may yield errors or infeasible
 # solutions.
@@ -59,6 +61,65 @@ def ll_test_model(request):
     test_model = construct_ll_test_model()
     test_model.solver = request.param
     return test_model
+
+
+def construct_papin_2003_model():
+    test_model = Model('papin_2003')
+    test_model.add_metabolites(Metabolite("A"))
+    test_model.add_metabolites(Metabolite("B"))
+    test_model.add_metabolites(Metabolite("C"))
+    test_model.add_metabolites(Metabolite("D"))
+    test_model.add_metabolites(Metabolite("E"))
+    test_model.add_metabolites(Metabolite("byp"))
+    test_model.add_metabolites(Metabolite("cof"))
+    v1 = Reaction("v1")
+    v1.add_metabolites({test_model.metabolites.A: -1.0,
+                        test_model.metabolites.B: 1.0})
+    v2 = Reaction("v2")
+    v2.add_metabolites({test_model.metabolites.B: -2.0,
+                        test_model.metabolites.C: 1.0,
+                        test_model.metabolites.byp: 1.0})
+    v3 = Reaction("v3")
+    v3.add_metabolites({test_model.metabolites.B: -2.0,
+                        test_model.metabolites.cof: -1.0,
+                        test_model.metabolites.D: 1.0,
+                        test_model.metabolites.byp: 1.0})
+    v4 = Reaction("v4")
+    v4.add_metabolites({test_model.metabolites.D: -1.0,
+                        test_model.metabolites.E: 1.0,
+                        test_model.metabolites.cof: 1.0})
+    v5 = Reaction("v5")
+    v5.add_metabolites({test_model.metabolites.C: -1.0,
+                        test_model.metabolites.cof: -1.0,
+                        test_model.metabolites.D: 1.0})
+    v6 = Reaction("v6", upper_bound=0.0)
+    v6.add_metabolites({test_model.metabolites.C: -1.0,
+                        test_model.metabolites.E: 1.0})
+    b1 = Reaction("b1", upper_bound=10.0, lower_bound=0.0)
+    b1.add_metabolites({test_model.metabolites.A: 1.0})
+    b2 = Reaction("b2")
+    b2.add_metabolites({test_model.metabolites.E: -1.0})
+    b3 = Reaction("b3")
+    b3.add_metabolites({test_model.metabolites.byp: -1.0})
+    test_model.add_reactions([v1, v2, v3, v4, v5, v6, b1, b2, b3])
+    test_model.objective = 'b2'
+    return test_model
+
+
+def construct_papin_2003_solution():
+    fluxes = Series({'b1': 10.0, 'b2': 5.0, 'b3': 5.0, 'v1': 10.0, 'v2': 5.0,
+                     'v3': 0.0, 'v4': 0.0, 'v5': 0.0, 'v6': 5.0})
+    reduced_costs = Series({'b1': 0.0, 'b2': 0.0, 'b3': 0.0, 'v1': 0.0,
+                            'v2': 0.0, 'v3': 0.0, 'v4': 0.0, 'v5': 0.0,
+                            'v6': 0.0})
+    shadow_prices = Series({'b1': 0.0, 'b2': 0.0, 'b3': 0.0, 'v1': 0.0,
+                            'v2': 0.0, 'v3': 0.0, 'v4': 0.0, 'v5': 0.0,
+                            'v6': 0.0})
+    sol = Solution(objective_value=5.000, status='optimal',
+                   fluxes=fluxes,
+                   reduced_costs=reduced_costs,
+                   shadow_prices=shadow_prices)
+    return sol
 
 
 @contextmanager
@@ -314,6 +375,25 @@ class TestCobraFluxAnalysis:
                                             solver):
         model.solver = solver
         benchmark(single_reaction_deletion, model=model, processes=1)
+
+    @pytest.mark.parametrize("solver", optlang_solvers)
+    def test_single_reaction_deletion_room(self, solver):
+        model = construct_papin_2003_model()
+        model.solver = solver
+        wt_sol = construct_papin_2003_solution()
+        expected = Series({'v1': 10.0, 'v2': 5.0, 'v3': 0.0, 'v4': 5.0,
+                           'v5': 5.0, 'v6': 0.0, 'b1': 10.0, 'b2': 5.0,
+                           'b3': 5.0})
+        with model:
+            add_room(model, solution=wt_sol, delta=0.0, epsilon=0.0)
+            room_sol = model.optimize()
+        with model:
+            add_room(model, solution=wt_sol, delta=0.0, epsilon=0.0,
+                     linear=True)
+            linear_room_sol = model.optimize()
+
+        assert expected.eq(room_sol.fluxes).all()
+        assert expected.eq(linear_room_sol.fluxes).all()
 
     @pytest.mark.parametrize("solver", optlang_solvers)
     def test_single_reaction_deletion(self, model, solver):
