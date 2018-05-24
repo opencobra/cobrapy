2e6db613153914649f6a864f8d35660609f3308c
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2018-05-25 00:55:03 +0200

    Bump version: 0.11.3 → 0.12.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ except IOError:
 
 setup(
     name="cobra",
-    version="0.11.3",
+    version="0.12.0",
     packages=find_packages(),
     setup_requires=setup_requirements,
     install_requires=[
