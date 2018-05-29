1ca1bf34508e7380e386935e1664ceab21359603
Christian Diener <ch.diener@gmail.com>
2018-05-29 14:11:03 -0500

    raise correct Error type

--- cobra/util/solver.py
+++ cobra/util/solver.py
@@ -407,7 +407,7 @@ def check_solver_status(status, raise_error=False):
     elif (status in has_primals) and not raise_error:
         warn("solver status is '{}'".format(status), UserWarning)
     elif status is None:
-        raise RuntimeError(
+        raise OptimizationError(
             "model was not optimized yet or solver context switched")
     else:
         raise OptimizationError("solver status is '{}'".format(status))
