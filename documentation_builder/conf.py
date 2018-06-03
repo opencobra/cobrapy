bca9a1d125a82b6ef8de16e47220a68a08e95c9b
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2018-06-04 00:58:31 +0200

    fix: restore sys.path

--- documentation_builder/conf.py
+++ documentation_builder/conf.py
@@ -18,7 +18,7 @@ from os.path import dirname
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-# sys.path.insert(0, dirname(dirname(__file__)))
+sys.path.insert(0, dirname(dirname(__file__)))
 
 
 # In order to build documentation that requires libraries to import
