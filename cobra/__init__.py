e1b337519ba42e47cb520c59b90041db084f62ce
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2018-07-20 11:59:10 +0200

    Bump version: 0.13.0 → 0.13.1

--- cobra/__init__.py
+++ cobra/__init__.py
@@ -13,7 +13,7 @@ from cobra.core import (
     DictList, Gene, Metabolite, Model, Object, Reaction, Species)
 from cobra.util import show_versions
 
-__version__ = "0.13.0"
+__version__ = "0.13.1"
 
 # set the warning format to be prettier and fit on one line
 _cobra_path = _dirname(_abspath(__file__))
