c78402befbeb0e70d01e68207a51309dd3f0557f
maureencarey <maureen.carey@mac.com>
2018-08-09 16:57:27 -0400

    update rename_genes to avoid adding unnecessary genes and to rename a subset of genes

--- cobra/manipulation/modify.py
+++ cobra/manipulation/modify.py
@@ -80,8 +80,10 @@ def rename_genes(cobra_model, rename_dict):
         new_gene_present = new_name in cobra_model.genes
         if old_gene_present and new_gene_present:
             old_gene = cobra_model.genes.get_by_id(old_name)
-            remove_genes.append(old_gene)
-            recompute_reactions.update(old_gene._reaction)
+            # Added in case not renaming some genes:
+            if old_gene != cobra_model.genes.get_by_id(new_name):
+                remove_genes.append(old_gene)
+                recompute_reactions.update(old_gene._reaction)
         elif old_gene_present and not new_gene_present:
             # rename old gene to new gene
             gene = cobra_model.genes[gene_index]
@@ -93,7 +95,10 @@ def rename_genes(cobra_model, rename_dict):
             pass
         else:  # not old gene_present and not new_gene_present
             # the new gene's _model will be set by repair
-            cobra_model.genes.append(Gene(new_name))
+            # This would add genes from rename_dict
+            # that are not associated with a rxn
+            # cobra_model.genes.append(Gene(new_name))
+            pass
     cobra_model.repair()
 
     class Renamer(NodeTransformer):
