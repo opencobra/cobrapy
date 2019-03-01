41945c7835a4d438854b32424a15e7dd0524b015
Matthias Koenig <konigmatt@googlemail.com>
2019-03-01 12:13:04 +0100

    working on annotation fixes

--- cobra/io/sbml.py
+++ cobra/io/sbml.py
@@ -923,6 +923,32 @@ def _check_required(sbase, value, attribute):
     return value
 
 
+def _check(value, message):
+    """
+    Checks the libsbml return value and prints message if something happened.
+
+    If 'value' is None, prints an error message constructed using
+      'message' and then exits with status code 1. If 'value' is an integer,
+      it assumes it is a libSBML return status code. If the code value is
+      LIBSBML_OPERATION_SUCCESS, returns without further action; if it is not,
+      prints an error message constructed using 'message' along with text from
+      libSBML explaining the meaning of the code, and exits with status code 1.
+
+    """
+    if value is None:
+        LOGGER.error('Error: LibSBML returned a null value trying '
+                     'to <' + message + '>.')
+    elif type(value) is int:
+        if value == libsbml.LIBSBML_OPERATION_SUCCESS:
+            return
+        else:
+            LOGGER.error('Error encountered trying to <' + message + '>.')
+            LOGGER.error('LibSBML returned error code {}: {}'.format(str(value),
+                         libsbml.OperationReturnValue_toString(value).strip()))
+    else:
+        return
+
+
 # ----------------------
 # Notes
 # ----------------------
@@ -965,7 +991,10 @@ def _sbase_notes_dict(sbase, notes):
         tokens = ['<html xmlns = "http://www.w3.org/1999/xhtml" >'] + \
             ["<p>{}: {}</p>".format(k, v) for (k, v) in notes.items()] + \
             ["</html>"]
-        sbase.setNotes("\n".join(tokens))
+        _check(
+            sbase.setNotes("\n".join(tokens)),
+            "Setting notes on sbase: {}".format(sbase)
+        )
 
 
 # ----------------------
@@ -1093,7 +1122,8 @@ def _sbase_annotations(sbase, annotation):
             if provider == "SBO":
                 logging.warning("'SBO' provider is deprecated, "
                                 "use 'sbo' provider instead")
-            sbase.setSBOTerm(identifiers)
+            _check(sbase.setSBOTerm(identifiers),
+                "Setting SBOTerm: {}".format(identifiers))
         else:
             if isinstance(identifiers, string_types):
                 identifiers = (identifiers,)
@@ -1111,7 +1141,8 @@ def _sbase_annotations(sbase, annotation):
                 cv.addResource("%s/%s/%s" % (URL_IDENTIFIERS_PREFIX, provider,
                                              identifier))
                 # FIXME: add a check that this worked
-                sbase.addCVTerm(cv)
+                _check(sbase.addCVTerm(cv),
+                       "Setting cvterm: {}".format(cv))
 
 
 # -----------------------------------
