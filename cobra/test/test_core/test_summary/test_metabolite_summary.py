d34dd3d59a15257585c56056b53649e49405e906
Synchon Mandal <synchonmandal@gmail.com>
2019-07-15 11:50:18 +0530

    update: update test_metabolite_summary.py

--- cobra/test/test_core/test_summary/test_metabolite_summary.py
+++ cobra/test/test_core/test_summary/test_metabolite_summary.py
@@ -17,7 +17,7 @@ def test_metabolite_summary_to_table_previous_solution(model, opt_solver, met):
     model.solver = opt_solver
     solution = pfba(model)
 
-    expected_entry = ['PRODUCING CYTBD    100%   43.6  2.0 h_c + 0.5 o2_c + '
+    expected_entry = ['PRODUCING CYTBD     100    43.6  2.0 h_c + 0.5 o2_c + '
                       'q8h2_c --> h2o_c + 2.0 h_...']
 
     with captured_output() as (out, _):
@@ -31,11 +31,11 @@ def test_metabolite_summary_to_frame_previous_solution(model, opt_solver, met):
     model.solver = opt_solver
     solution = pfba(model)
 
-    expected_percent = ['100%', '88%', '12%']
+    expected_percent = [100.0, 88.4, 11.6]
 
     out_df = model.metabolites.get_by_id(met).summary(solution).to_frame()
 
-    assert out_df['PERCENT'].tolist() == expected_percent
+    assert out_df['PERCENT'].round(1).tolist() == expected_percent
 
 
 @pytest.mark.parametrize("met, names", [
@@ -48,11 +48,12 @@ def test_metabolite_summary_to_table(model, opt_solver, met, names):
     model.optimize()
 
     if names:
-        expected_entry = ['PRODUCING cytochr...   100%   43.6  2.0 H+ + '
-                          '0.5 O2 + Ubiquinol-8 --> H2O + 2.0 H+...']
+        expected_entry = ['PRODUCING cytochrome oxidase bd '
+                          '(ubiquinol-8: 2 protons)    100    43.6  '
+                          '2.0 H+ + 0.5 O2 + Ubiquinol-8 --> H2O + 2.0 H+...']
     else:
-        expected_entry = ['PRODUCING CYTBD    100%   43.6  2.0 h_c + 0.5 '
-                          'o2_c + q8h2_c --> h2o_c + 2.0 h_...']
+        expected_entry = ['PRODUCING CYTBD     100    43.6  2.0 h_c + '
+                          '0.5 o2_c + q8h2_c --> h2o_c + 2.0 h_...']
 
     with captured_output() as (out, _):
         print(model.metabolites.get_by_id(met).summary(names=names))
@@ -68,11 +69,11 @@ def test_metabolite_summary_to_frame(model, opt_solver, met, names):
     model.solver = opt_solver
     model.optimize()
 
-    expected_percent = ['100%', '88%', '12%']
+    expected_percent = [100.0, 88.4, 11.6]
 
     out_df = model.metabolites.get_by_id(met).summary(names=names).to_frame()
 
-    assert out_df['PERCENT'].tolist() == expected_percent
+    assert out_df['PERCENT'].round(1).tolist() == expected_percent
 
 
 @pytest.mark.parametrize("fraction, met", [(0.99, "fdp_c")])
@@ -84,7 +85,7 @@ def test_metabolite_summary_to_table_with_fva(model, opt_solver, fraction,
     model.solver = opt_solver
     model.optimize()
 
-    expected_entry = ['PRODUCING PFK   100%   7.48    6.17      9.26    '
+    expected_entry = ['PRODUCING PFK   100     7.48    6.17      9.26    '
                       'atp_c + f6p_c --> adp_c + fdp_c + h_c']
 
     with captured_output() as (out, _):
@@ -100,7 +101,7 @@ def test_metabolite_summary_to_frame_with_fva(model, opt_solver, fraction,
     model.solver = opt_solver
     model.optimize()
 
-    expected_percent = ['100%', '100%', '0%']
+    expected_percent = [100.0, 100.0, 0.0]
 
     out_df = model.metabolites.get_by_id(met).summary(fva=fraction).to_frame()
 
