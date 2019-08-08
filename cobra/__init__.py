7a8ebca27006597f5818e0e46917cc9201e9c5fe
Midnighter <midnighter@posteo.net>
2019-08-09 00:47:03 +0200

    chore: include Python 2 warning

--- cobra/__init__.py
+++ cobra/__init__.py
@@ -8,16 +8,6 @@ from os import name as _name
 from os.path import abspath as _abspath
 from os.path import dirname as _dirname
 
-from cobra.core import (
-    Configuration, DictList, Gene, Metabolite, Model, Object, Reaction,
-    Solution, Species)
-from cobra import flux_analysis
-from cobra import io
-from cobra import sampling
-from cobra.util import show_versions
-
-__version__ = "0.15.4"
-
 # set the warning format to be prettier and fit on one line
 _cobra_path = _dirname(_abspath(__file__))
 if _name == "posix":
@@ -29,4 +19,28 @@ else:
 def _warn_format(message, category, filename, lineno, file=None, line=None):
     shortname = filename.replace(_cobra_path, "cobra", 1)
     return _warning_base % (shortname, lineno, category.__name__, message)
+
 _warnings.formatwarning = _warn_format
+
+import sys
+
+if sys.version_info[0] == 2:
+    _warnings.warn(
+        "Python 2 is reaching end of life (see "
+        "https://www.python.org/dev/peps/pep-0373/) and many cobra "
+        "dependencies have already dropped support. At the moment it *should* "
+        "still work but we will no longer actively maintain Python 2 support.",
+        FutureWarning
+    )
+
+from cobra.core import (
+    Configuration, DictList, Gene, Metabolite, Model, Object, Reaction,
+    Solution, Species)
+from cobra import flux_analysis
+from cobra import io
+from cobra import sampling
+from cobra.util import show_versions
+
+__version__ = "0.15.4"
+
+
