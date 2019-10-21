338d67399bc595831322131791949c118b219c4c
Christian Diener <ch.diener@gmail.com>
2019-10-21 13:08:03 -0700

    fix isort issues

--- cobra/flux_analysis/variability.py
+++ cobra/flux_analysis/variability.py
@@ -13,9 +13,7 @@ from pandas import DataFrame
 
 from cobra.core import Configuration, get_solution
 from cobra.flux_analysis.deletion import (
-    single_gene_deletion,
-    single_reaction_deletion,
-)
+    single_gene_deletion, single_reaction_deletion)
 from cobra.flux_analysis.helpers import normalize_cutoff
 from cobra.flux_analysis.loopless import loopless_fva_iter
 from cobra.flux_analysis.parsimonious import add_pfba
