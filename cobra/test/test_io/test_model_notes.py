19428e61312d17343691cd7c86ab8a00f637bb90
HemantYadav <hy27.1999@gmail.com>
2020-03-15 15:00:33 +0530

    added test for checking model notes

---
+++ cobra/test/test_io/test_model_notes.py
@@ -0,0 +1,31 @@
+import pytest
+import cobra
+from os.path import join
+from cobra.util.solver import set_objective
+from cobra.io import read_sbml_model, write_sbml_model
+
+
+def test_model_notes(tmp_path):
+    """Testing if model notes are written in SBML"""
+    path_to_file = join(str(tmp_path), "model_notes.xml")
+
+    # making a minimal cobra model to test notes
+    model = cobra.Model("e_coli_core")
+    model.notes["Remark"] = "...Model Notes..."
+    met = cobra.Metabolite("pyr_c", compartment="c")
+    model.add_metabolites([met])
+    met.notes["Remark"] = "I appear."
+    rxn = cobra.Reaction("R_ATPM")
+    model.add_reactions([rxn])
+    rxn.notes["Remark"] = "What about me?"
+    model.objective_direction = "max"
+    coefficients = {}
+    coefficients[rxn] = 1
+    set_objective(model, coefficients)
+    write_sbml_model(model, path_to_file)
+
+    # reading the model back
+    model_after_reading = read_sbml_model(path_to_file)
+
+    # checking if notes are written to model
+    assert model_after_reading.notes["Remark"] == "...Model Notes..."
