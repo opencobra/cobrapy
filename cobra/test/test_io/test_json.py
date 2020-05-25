3961f11080bc8c94213064e9d0c452100f9f05bf
HemantYadav <hy27.1999@gmail.com>
2020-05-25 22:29:35 +0530

    modified json schema and put into a separate file

--- cobra/test/test_io/test_json.py
+++ cobra/test/test_io/test_json.py
@@ -10,17 +10,17 @@ from os.path import join
 import pytest
 
 import cobra.io as cio
+from cobra.io.json_schemas.json_schema_v1 import json_schema_V1
 from cobra.test.test_io.conftest import compare_models
 
 
-@pytest.mark.xfail(reason="schema outdated")
 def test_validate_json(data_directory):
     """Validate file according to JSON-schema."""
     jsonschema = pytest.importorskip("jsonschema")
     with open(join(data_directory, "mini.json"),
-              "r", encoding="utf-8") as infile:
+              "r") as infile:
         loaded = json.load(infile)
-    assert jsonschema.validate(loaded, cio.json.json_schema)
+    assert jsonschema.validate(loaded, json_schema_V1) is None
 
 
 def test_load_json_model(data_directory, mini_model):
@@ -29,13 +29,12 @@ def test_load_json_model(data_directory, mini_model):
     assert compare_models(mini_model, json_model) is None
 
 
-@pytest.mark.xfail(reason="schema outdated")
 def test_save_json_model(tmpdir, mini_model):
     """Test the writing of JSON model."""
     jsonschema = pytest.importorskip("jsonschema")
     output_file = tmpdir.join("mini.json")
     cio.save_json_model(mini_model, output_file.strpath, pretty=True)
     # validate against JSONSchema
-    with open(output_file, "r") as infile:
+    with open(output_file.strpath, "r") as infile:
         loaded = json.load(infile)
-    assert jsonschema.validate(loaded, cio.json.json_schema)
+    assert jsonschema.validate(loaded, json_schema_V1) is None
