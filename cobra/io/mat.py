fcafa3ecee0ea0835f75855d68dd7d3aab79a49b
weilandtd <daniel.weilandt@epfl.ch>
2020-05-26 20:24:27 +0200

    FIX: Allow comparment names with two letters
    New yeast models are provided with 2 letter compartment names.  Currently import from matlab model does not work.

--- cobra/io/mat.py
+++ cobra/io/mat.py
@@ -25,18 +25,18 @@ except ImportError:
 
 
 # precompiled regular expressions
-_bracket_re = re.compile(r"\[[a-z]\]$")
-_underscore_re = re.compile(r"_[a-z]$")
+_bracket_re = re.compile(r"\[[a-z]{1,2}\]$")
+_underscore_re = re.compile(r"_[a-z]{1,2}$")
 
 
 def _get_id_compartment(id):
     """extract the compartment from the id string"""
     bracket_search = _bracket_re.findall(id)
     if len(bracket_search) == 1:
-        return bracket_search[0][1]
+        return bracket_search[0][1:]
     underscore_search = _underscore_re.findall(id)
     if len(underscore_search) == 1:
-        return underscore_search[0][1]
+        return underscore_search[0][1:]
     return None
 
 
