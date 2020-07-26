f839755df4f6a0afd8adaa6dbe8d32eab4220a1d
HemantYadav <hy27.1999@gmail.com>
2020-07-26 19:25:15 +0530

    moved cobra directory to src directory

--- src/cobra/flux_analysis/__init__.py
+++ src/cobra/flux_analysis/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from cobra.flux_analysis.deletion import (
+from src.cobra.flux_analysis.deletion import (
     double_gene_deletion, double_reaction_deletion, single_gene_deletion,
     single_reaction_deletion)
 from cobra.flux_analysis.fastcc import fastcc
@@ -9,8 +9,5 @@ from cobra.flux_analysis.geometric import geometric_fba
 from cobra.flux_analysis.loopless import (loopless_solution, add_loopless)
 from cobra.flux_analysis.moma import add_moma, moma
 from cobra.flux_analysis.parsimonious import pfba
-from cobra.flux_analysis.variability import (
-    find_blocked_reactions, find_essential_genes, find_essential_reactions,
-    flux_variability_analysis)
 from cobra.flux_analysis.phenotype_phase_plane import production_envelope
 from cobra.flux_analysis.room import add_room, room
