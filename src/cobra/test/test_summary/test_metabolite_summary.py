3e6d6561cc6df0efaa7e0e7f0f7a424699bdf91e
Midnighter <midnighter@posteo.net>
2020-08-11 16:20:44 +0200

    tests: simplify unit tests
    * Test public class interface

--- src/cobra/test/test_summary/test_metabolite_summary.py
+++ src/cobra/test/test_summary/test_metabolite_summary.py
@@ -1,108 +1,106 @@
-# -*- coding: utf-8 -*-
+"""Unit test the MetaboliteSummary class."""
 
-"""Test functionalities of MetaboliteSummary."""
 
-from __future__ import absolute_import
-
-import numpy as np
 import pytest
 
-from cobra.flux_analysis.parsimonious import pfba
-from cobra.test.test_summary import captured_output, check_in_line, check_line
+from cobra.flux_analysis import flux_variability_analysis, pfba
+from cobra.summary import MetaboliteSummary
 
 
-@pytest.mark.parametrize("met", ["q8_c"])
-def test_metabolite_summary_to_table_previous_solution(model, opt_solver, met):
-    """Test metabolite summary._to_table() of previous solution."""
+def test_metabolite_summary_interface(model, opt_solver):
+    """Test that a summary can be created successfully."""
     model.solver = opt_solver
-    solution = pfba(model)
+    metabolite = model.metabolites.get_by_id("q8_c")
+    MetaboliteSummary(
+        metabolite=metabolite, model=model,
+    )
+    MetaboliteSummary(
+        metabolite=metabolite, model=model, solution=pfba(model),
+    )
+    MetaboliteSummary(
+        metabolite=metabolite, model=model, fva=0.95,
+    )
+    MetaboliteSummary(
+        metabolite=metabolite,
+        model=model,
+        fva=flux_variability_analysis(
+            model, reaction_list=["CYTBD", "NADH16", "SUCDi"]
+        ),
+    )
 
-    expected_entry = [
-        "PRODUCING CYTBD     100    43.6  2.0 h_c + 0.5 o2_c + "
-        "q8h2_c --> h2o_c + 2.0 h_"
-    ]
 
-    with captured_output() as (out, _):
-        print(model.metabolites.get_by_id(met).summary(solution))
-        check_in_line(out.getvalue(), expected_entry)
-
-
-@pytest.mark.parametrize("met", ["q8_c"])
-def test_metabolite_summary_to_frame_previous_solution(model, opt_solver, met):
-    """Test metabolite summary.to_frame() of previous solution."""
+def test_metabolite_summary_to_frame(model, opt_solver):
+    """Test that the summary's method ``to_frame`` can be called."""
     model.solver = opt_solver
-    solution = pfba(model)
-
-    expected_percent = [100.0, 88.4, 11.6]
-
-    out_df = model.metabolites.get_by_id(met).summary(solution).to_frame()
-
-    assert out_df["PERCENT"].round(1).tolist() == expected_percent
+    summary = model.metabolites.get_by_id("atp_c").summary()
+    summary.to_frame()
 
 
-@pytest.mark.parametrize("met, names", [("q8_c", False), ("q8_c", True)])
-def test_metabolite_summary_to_table(model, opt_solver, met, names):
-    """Test metabolite summary._to_table()."""
+@pytest.mark.parametrize(
+    "kwargs",
+    [
+        {},
+        {"names": True},
+        {"float_format": ".1f"},
+        {"threshold": 1e-2},
+        {"column_width": 20},
+    ],
+)
+def test_metabolite_summary_to_string(model, opt_solver, kwargs):
+    """Test that the summary's method ``to_string`` can be called."""
     model.solver = opt_solver
-    model.optimize()
-
-    if names:
-        expected_entry = [
-            "PRODUCING cytochrome oxidase bd "
-            "(ubiquinol-8: 2 protons)    100    43.6  "
-            "2.0 H+ + 0.5 O2 + Ubiquinol-8 --> H2O + 2.0 H+"
-        ]
-    else:
-        expected_entry = [
-            "PRODUCING CYTBD     100    43.6  2.0 h_c + "
-            "0.5 o2_c + q8h2_c --> h2o_c + 2.0 h_"
-        ]
-
-    with captured_output() as (out, _):
-        print(model.metabolites.get_by_id(met).summary(names=names))
-        check_in_line(out.getvalue(), expected_entry)
+    summary = model.metabolites.get_by_id("atp_c").summary()
+    summary.to_string(**kwargs)
 
 
-@pytest.mark.parametrize("met, names", [("q8_c", False), ("q8_c", True)])
-def test_metabolite_summary_to_frame(model, opt_solver, met, names):
-    """Test metabolite summary.to_frame()."""
+@pytest.mark.parametrize(
+    "kwargs", [{}, {"names": True}, {"float_format": ".1f"}, {"threshold": 1e-2}]
+)
+def test_metabolite_summary_to_html(model, opt_solver, kwargs):
+    """Test that the summary's method ``to_html`` can be called."""
     model.solver = opt_solver
-    model.optimize()
-
-    expected_percent = [100.0, 88.4, 11.6]
-
-    out_df = model.metabolites.get_by_id(met).summary(names=names).to_frame()
-
-    assert out_df["PERCENT"].round(1).tolist() == expected_percent
+    summary = model.metabolites.get_by_id("atp_c").summary()
+    summary.to_html(**kwargs)
 
 
-@pytest.mark.parametrize("fraction, met", [(0.99, "fdp_c")])
-def test_metabolite_summary_to_table_with_fva(model, opt_solver, fraction, met):
-    """Test metabolite summary._to_table() (using FVA)."""
-    #     pytest.xfail("FVA currently buggy")
-
+def test_q8_producing_summary(model, opt_solver):
+    """Test that the production summary of q8 is accurate."""
     model.solver = opt_solver
-    model.optimize()
+    summary = model.metabolites.get_by_id("q8_c").summary()
 
-    expected_entry = [
-        "PRODUCING PFK   100     7.48    6.17      9.26    "
-        "atp_c + f6p_c --> adp_c + fdp_c + h_c"
-    ]
-
-    with captured_output() as (out, _):
-        print(model.metabolites.get_by_id(met).summary(fva=fraction))
-        check_line(out.getvalue(), expected_entry)
+    assert summary.producing_fluxes.at["CYTBD", "percent"] == 1
+    assert summary.producing_fluxes.at["CYTBD", "flux"] == pytest.approx(43.6, abs=1e-2)
 
 
-@pytest.mark.parametrize("fraction, met", [(0.99, "fdp_c")])
-def test_metabolite_summary_to_frame_with_fva(model, opt_solver, fraction, met):
-    """Test metabolite summary.to_frame() (using FVA)."""
-
+def test_q8_consuming_summary(model, opt_solver):
+    """Test that the consumption summary of q8 is accurate."""
     model.solver = opt_solver
-    model.optimize()
+    summary = model.metabolites.get_by_id("q8_c").summary()
 
-    expected_percent = [100.0, 100.0, 0.0]
+    assert summary.consuming_fluxes.at["NADH16", "percent"] == pytest.approx(
+        0.8838, abs=1e-4
+    )
+    assert summary.consuming_fluxes.at["NADH16", "flux"] == pytest.approx(
+        -38.54, abs=1e-2
+    )
 
-    out_df = model.metabolites.get_by_id(met).summary(fva=fraction).to_frame()
+    assert summary.consuming_fluxes.at["SUCDi", "percent"] == pytest.approx(
+        0.1162, abs=1e-4
+    )
+    assert summary.consuming_fluxes.at["SUCDi", "flux"] == pytest.approx(
+        -5.06, abs=1e-2
+    )
 
-    assert np.allclose(out_df["PERCENT"].tolist(), expected_percent, 1e-6, 1e-6)
+
+def test_fdp_production_with_fva(model, opt_solver):
+    """Test that the production summary of fdp is within expected bounds."""
+    model.solver = opt_solver
+    summary = model.metabolites.get_by_id("fdp_c").summary(fva=0.99)
+    assert summary.producing_fluxes.at["PFK", "percent"] == 1
+    assert summary.producing_fluxes.at["PFK", "flux"] == pytest.approx(7.48, abs=1e-2)
+    assert summary.producing_fluxes.at["PFK", "minimum"] == pytest.approx(
+        6.17, abs=1e-2
+    )
+    assert summary.producing_fluxes.at["PFK", "maximum"] == pytest.approx(
+        9.26, abs=1e-2
+    )
