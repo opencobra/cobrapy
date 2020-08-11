9a310322f55e340c1ffd7d268817fa7bde61cebd
Midnighter <midnighter@posteo.net>
2020-08-11 22:31:15 +0200

    refactor: docstrings and attributes

--- src/cobra/test/test_summary/test_model_summary.py
+++ src/cobra/test/test_summary/test_model_summary.py
@@ -59,26 +59,28 @@ def test_model_summary_to_html(model, opt_solver, kwargs):
     summary.to_html(**kwargs)
 
 
-@pytest.mark.parametrize("names", [False, True])
-def test_model_summary_to_frame_previous_solution(model, opt_solver, names):
-    """Test Summary._to_table() of previous solution."""
+def test_model_summary_to_frame_previous_solution(model, opt_solver):
+    """Test that the summary correctly uses an existing solution."""
     model.solver = opt_solver
     solution = pfba(model)
-    rxn_test = model.reactions.EX_glc__D_e
+    rxn = model.reactions.EX_glc__D_e
+    solution.fluxes[rxn.id] = -123
     summary = model.summary(solution=solution)
-    assert summary.to_frame().at[rxn_test.id, "flux"] == 10
+    assert summary.to_frame().at[rxn.id, "flux"] == 123
 
 
 def test_model_summary_flux(model, opt_solver):
-    """Test model.summary()._to_table()."""
+    """Test that the summary has expected fluxes."""
     model.solver = opt_solver
     summary = model.summary()
-    assert summary.to_frame().at["EX_o2_e", "flux"] == pytest.approx(21.8, abs=1e-02)
-    assert summary.to_frame().at["EX_h2o_e", "flux"] == pytest.approx(-29.18, abs=1e-02)
+    assert summary.uptake_flux.at["EX_o2_e", "flux"] == pytest.approx(21.8, abs=1e-02)
+    assert summary.secretion_flux.at["EX_h2o_e", "flux"] == pytest.approx(
+        -29.18, abs=1e-02
+    )
 
 
 def test_model_summary_fva(model, opt_solver):
-    """Test model summary._to_table() (using FVA)."""
+    """Test that the exchange summary is within expected bounds."""
     model.solver = opt_solver
     summary = model.summary(fva=0.95)
     assert summary.to_frame().at["EX_o2_e", "flux"] == pytest.approx(21.8, abs=1e-02)
