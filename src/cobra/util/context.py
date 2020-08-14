43ca7855527932b015785088b09bc3f5c0685509
Synchon Mandal <synchonmandal@gmail.com>
2020-08-14 09:55:40 +0200

    refactor: update docstrings and add type annotations for cobra.util.context

--- src/cobra/util/context.py
+++ src/cobra/util/context.py
@@ -1,46 +1,68 @@
 """Context manager for the package."""
 
 from functools import partial
+from typing import Any, Callable
 
 
 class HistoryManager:
-    """Record a list of actions to be taken at a later time. Used to
-    implement context managers that allow temporary changes to a
-    :class:`~cobra.core.Model`.
+    """
+    Define a base context manager.
+
+    It records a list of actions to be taken at a later time.
+    This is used to implement context managers that allow temporary
+    changes to a `cobra.core.Model`.
 
     """
 
     def __init__(self):
-
-        # self._history just acts as a stack
+        """Initialize the class."""
+        # this acts like a stack
         self._history = []
 
-    def __call__(self, operation):
-        """Add the corresponding method to the history stack.
+    def __call__(self, operation: Callable[[Any], Any]):
+        """Add the corresponding operation to the history stack.
 
         Parameters
         ----------
-        operation : `function`
-            A function to be called at a later time
+        operation : callable
+            A function to be called at a later time.
 
         """
-
         self._history.append(operation)
 
     def reset(self):
-        """Trigger executions for all items in the stack in reverse order"""
+        """Trigger executions for all items in the stack in reverse order."""
         while self._history:
             entry = self._history.pop()
             entry()
 
 
 def get_context(obj):
-    """Search for a context manager"""
+    """Search for a context manager.
+
+    Parameters
+    ----------
+    obj: cobra.Object
+        The cobra.Object for which to search context manager.
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    AttributeError
+        If no context manager is found.
+    IndexError
+        If no context manager is found.
+
+    """
+    # works for cobra.core.Model objects
     try:
         return obj._contexts[-1]
     except (AttributeError, IndexError):
         pass
-
+    # works for objects other than cobra.core.Model
     try:
         return obj._model._contexts[-1]
     except (AttributeError, IndexError):
@@ -49,12 +71,25 @@ def get_context(obj):
     return None
 
 
-def resettable(f):
-    """A decorator to simplify the context management of simple object
-    attributes. Gets the value of the attribute prior to setting it, and stores
-    a function to set the value to the old value in the HistoryManager.
+def resettable(f: Callable[[Any], Any]):
     """
+    Simplify the context management of simple object attributes.
 
+    It gets the value of the attribute prior to setting it, and stores
+    a function to set the value to the old value in the
+    `cobra.util.HistoryManager`.
+
+    Parameters
+    ----------
+    f: callable
+        The function to decorate.
+
+    Returns
+    -------
+    callable
+        The decorated function.
+
+    """
     def wrapper(self, new_value):
         context = get_context(self)
         if context:
