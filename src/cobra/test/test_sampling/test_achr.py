3b588bd0a0efd9924167327ecc000e3220e88207
Synchon Mandal <synchonmandal@gmail.com>
2020-09-15 15:11:54 +0200

    refactor: update test_achr.py

--- src/cobra/test/test_sampling/test_achr.py
+++ src/cobra/test/test_sampling/test_achr.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
-
 """Test functionalities of ACHRSampler."""
 
-from __future__ import absolute_import
+from typing import TYPE_CHECKING
 
 import numpy as np
 import pytest
@@ -10,21 +8,22 @@ import pytest
 from cobra.sampling import ACHRSampler
 
 
-def test_achr_init_benchmark(model, benchmark):
-    """Benchmark inital ACHR sampling."""
+if TYPE_CHECKING:
+    from cobra import Model
 
+
+def test_achr_init_benchmark(model: "Model", benchmark) -> None:
+    """Benchmark inital ACHR sampling."""
     benchmark(lambda: ACHRSampler(model))
 
 
-def test_achr_sample_benchmark(achr, benchmark):
+def test_achr_sample_benchmark(achr: ACHRSampler, benchmark) -> None:
     """Benchmark ACHR sampling."""
-
     benchmark(achr.sample, 1)
 
 
-def test_validate_wrong_sample(achr, model):
+def test_validate_wrong_sample(achr: ACHRSampler, model: "Model") -> None:
     """Test sample correctness."""
-
     s = achr.sample(10)
     s["hello"] = 1
 
@@ -32,23 +31,20 @@ def test_validate_wrong_sample(achr, model):
         achr.validate(s)
 
 
-def test_sampling(achr):
+def test_sampling(achr: ACHRSampler) -> None:
     """Test sampling."""
-
     s = achr.sample(10)
     assert all(achr.validate(s) == "v")
 
 
-def test_batch_sampling(achr):
+def test_batch_sampling(achr: ACHRSampler) -> None:
     """Test batch sampling."""
-
     for b in achr.batch(5, 4):
         assert all(achr.validate(b) == "v")
 
 
-def test_variables_samples(achr):
+def test_variables_samples(achr: ACHRSampler) -> None:
     """Test variable samples."""
-
     vnames = np.array([v.name for v in achr.model.variables])
     s = achr.sample(10, fluxes=False)
     assert s.shape == (10, achr.warmup.shape[1])
