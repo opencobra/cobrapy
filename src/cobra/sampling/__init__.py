e90afc128b9ccacddcda8fe02af3f35b54d4282d
Synchon Mandal <synchonmandal@gmail.com>
2021-03-19 20:50:20 +0530

    refactor: adapt sampling/__init__.py to export step() from core.py

--- src/cobra/sampling/__init__.py
+++ src/cobra/sampling/__init__.py
@@ -1,4 +1,5 @@
-from .hr_sampler import HRSampler, shared_np_array, step
+from .hr_sampler import HRSampler, shared_np_array
 from .achr import ACHRSampler
+from .core import step
 from .optgp import OptGPSampler
 from .sampling import sample
