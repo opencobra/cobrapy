7c4a96b06a4b51de912088c59af85004333168e1
Synchon Mandal <synchonmandal@gmail.com>
2021-03-21 17:20:09 +0530

    refactor: add typing annotations and docstrings in cobra.test.test_sampling.test_optgp

--- src/cobra/test/test_sampling/test_optgp.py
+++ src/cobra/test/test_sampling/test_optgp.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
-
 """Test functionalities of OptGPSampler."""
 
-from __future__ import absolute_import
+from typing import TYPE_CHECKING
 
 import numpy as np
 import pytest
@@ -10,10 +8,14 @@ import pytest
 from cobra.sampling import OptGPSampler
 
 
-@pytest.fixture(scope="function")
-def optgp(model):
-    """Return OptGPSampler instance for tests."""
+if TYPE_CHECKING:
+    from cobra import Model
+    from cobra.sampling import ACHRSampler
 
+
+@pytest.fixture(scope="function")
+def optgp(model: "Model") -> OptGPSampler:
+    """Return OptGPSampler instance for tests."""
     sampler = OptGPSampler(model, processes=1, thinning=1)
     assert (sampler.n_warmup > 0) and (sampler.n_warmup <= 2 * len(model.variables))
     assert all(sampler.validate(sampler.warmup) == "v")
@@ -21,35 +23,30 @@ def optgp(model):
     return sampler
 
 
-def test_optgp_init_benchmark(model, benchmark):
+def test_optgp_init_benchmark(model: "Model", benchmark) -> None:
     """Benchmark inital OptGP sampling."""
-
     benchmark(lambda: OptGPSampler(model, processes=2))
 
 
-def test_optgp_sample_benchmark(optgp, benchmark):
+def test_optgp_sample_benchmark(optgp: "Model", benchmark) -> None:
     """Benchmark OptGP sampling."""
-
     benchmark(optgp.sample, 1)
 
 
-def test_sampling(optgp):
+def test_sampling(optgp: OptGPSampler) -> None:
     """Test sampling."""
-
     s = optgp.sample(10)
     assert all(optgp.validate(s) == "v")
 
 
-def test_batch_sampling(optgp):
+def test_batch_sampling(optgp: OptGPSampler) -> None:
     """Test batch sampling."""
-
     for b in optgp.batch(5, 4):
         assert all(optgp.validate(b) == "v")
 
 
-def test_variables_samples(achr, optgp):
+def test_variables_samples(achr: "ACHRSampler", optgp: OptGPSampler) -> None:
     """Test variable samples."""
-
     vnames = np.array([v.name for v in achr.model.variables])
     s = optgp.sample(10, fluxes=False)
     assert s.shape == (10, optgp.warmup.shape[1])
@@ -57,9 +54,8 @@ def test_variables_samples(achr, optgp):
     assert (optgp.validate(s) == "v").all()
 
 
-def test_reproject(optgp):
+def test_reproject(optgp: OptGPSampler) -> None:
     """Test reprojection of sampling."""
-
     s = optgp.sample(10, fluxes=False).values
     proj = np.apply_along_axis(optgp._reproject, 1, s)
     assert all(optgp.validate(proj) == "v")
