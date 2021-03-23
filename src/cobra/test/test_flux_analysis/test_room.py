26da821476e0feea51773a5b1b4bbc820f9170ad
Christian Diener <ch.diener@gmail.com>
2021-03-23 12:23:29 -0700

    more appropriate ROOM test

--- src/cobra/test/test_flux_analysis/test_room.py
+++ src/cobra/test/test_flux_analysis/test_room.py
@@ -4,12 +4,16 @@
 
 from __future__ import absolute_import
 
-import numpy as np
+import pytest
 
 from cobra.flux_analysis.room import add_room
 
 
-def test_room_sanity(model, all_solvers):
+@pytest.mark.parametrize("linear, delta, eps", [
+    (True, 0.03, 1e-3), (False, 0.03, 1e-3),
+    (True, 0.1, 1e-2), (False, 0.1, 1e-2)
+])
+def test_room_sanity(model, all_solvers, linear, delta, eps):
     """Test optimization criterion and optimality for ROOM."""
     model.solver = all_solvers
     sol = model.optimize()
@@ -18,62 +22,26 @@ def test_room_sanity(model, all_solvers):
         knock_sol = model.optimize()
 
     with model:
-        # Internally uses pFBA as reference solution.
-        add_room(model)
+        # let it calculate its own reference solution (pFBA)
+        add_room(model, linear=linear, delta=delta, epsilon=eps)
         model.reactions.PYK.knock_out()
         room_sol = model.optimize()
 
     with model:
-        # Use FBA as reference solution.
-        add_room(model, solution=sol)
+        # use the more distant FBA reference
+        add_room(model, solution=sol, linear=linear, delta=delta, epsilon=eps)
         model.reactions.PYK.knock_out()
         room_sol_ref = model.optimize()
 
-    flux_change = (sol.fluxes - knock_sol.fluxes).abs().sum()
-    flux_change_room = (sol.fluxes - room_sol.fluxes).abs().sum()
-    flux_change_room_ref = (sol.fluxes - room_sol_ref.fluxes).abs().sum()
-    # Expect the ROOM solution to have smaller flux changes in
-    # reactions compared to a normal FBA.
-    assert flux_change_room < flux_change or np.isclose(
-        flux_change_room, flux_change, atol=1e-06
-    )
-    # Expect the FBA-based reference to have less change in
-    # flux distribution.
-    assert flux_change_room_ref > flux_change_room or np.isclose(
-        flux_change_room_ref, flux_change_room, atol=1e-06
-    )
+    # The
+    flux_change = (sol.fluxes - knock_sol.fluxes).abs()
+    flux_change_room = (sol.fluxes - room_sol.fluxes).abs()
+    flux_change_room_ref = (sol.fluxes - room_sol_ref.fluxes).abs()
+    rxn_count_naive = (flux_change > delta * sol.fluxes.abs() + eps + 1e-6).sum()
+    rxn_count_room = (flux_change_room > delta * sol.fluxes.abs() + eps + 1e-6).sum()
+    rxn_count_room_ref = (
+        flux_change_room_ref > delta * sol.fluxes.abs() + eps + 1e-6).sum()
 
-
-def test_linear_room_sanity(model, all_solvers):
-    """Test optimization criterion and optimality for linear ROOM."""
-    model.solver = all_solvers
-    sol = model.optimize()
-    with model:
-        model.reactions.PYK.knock_out()
-        knock_sol = model.optimize()
-
-    with model:
-        # Internally uses pFBA as reference solution.
-        add_room(model, linear=True)
-        model.reactions.PYK.knock_out()
-        room_sol = model.optimize()
-
-    with model:
-        # Use FBA as reference solution.
-        add_room(model, solution=sol, linear=True)
-        model.reactions.PYK.knock_out()
-        room_sol_ref = model.optimize()
-
-    flux_change = (sol.fluxes - knock_sol.fluxes).abs().sum()
-    flux_change_room = (sol.fluxes - room_sol.fluxes).abs().sum()
-    flux_change_room_ref = (sol.fluxes - room_sol_ref.fluxes).abs().sum()
-    # Expect the ROOM solution to have smaller flux changes in
-    # reactions compared to a normal FBA.
-    assert flux_change_room < flux_change or np.isclose(
-        flux_change_room, flux_change, atol=1e-06
-    )
-    # Expect the FBA-based reference to have less change in
-    # flux distribution.
-    assert flux_change_room_ref > flux_change_room or np.isclose(
-        flux_change_room_ref, flux_change_room, atol=1e-06
-    )
+    # Expect the ROOM solution to have less changed reactions then a pFBA or FBA
+    assert rxn_count_room <= rxn_count_naive
+    assert rxn_count_room_ref <= rxn_count_naive
