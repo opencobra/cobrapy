36402fc527a724f5e99e38a197fd3fc10fc30e4b
Synchon Mandal <synchonmandal@gmail.com>
2021-04-06 23:28:41 +0530

    refactor: add type annotations and make a few cosmetic changes in boundary_types.py

--- src/cobra/medium/boundary_types.py
+++ src/cobra/medium/boundary_types.py
@@ -1,43 +1,54 @@
-# -*- coding: utf-8 -*-
-
-"""Contains function to identify the type of boundary reactions.
+"""Provide functions to identify the type of boundary reactions.
 
 This module uses various heuristics to decide whether a boundary reaction
-is an exchange, demand or sink reaction. It mostly orientates on the following
-paper:
+is an exchange, demand or sink reaction. It mostly orientates on the
+following paper:
 
 Thiele, I., & Palsson, B. Ø. (2010, January). A protocol for
 generating a high-quality genome-scale metabolic reconstruction.
 Nature protocols. Nature Publishing Group.
 http://doi.org/10.1038/nprot.2009.203
+
 """
 
 import logging
-from collections import Counter
+from typing import TYPE_CHECKING, List, Optional
 
 import pandas as pd
 
-from cobra.medium.annotations import compartment_shortlist, excludes, sbo_terms
+from .annotations import compartment_shortlist, excludes, sbo_terms
 
 
-LOGGER = logging.getLogger(__name__)
+if TYPE_CHECKING:
+    from cobra import Model, Reaction
 
 
-def find_external_compartment(model):
+logger = logging.getLogger(__name__)
+
+
+def find_external_compartment(model: "Model") -> str:
     """Find the external compartment in the model.
 
-    Uses a simple heuristic where the external compartment should be the one
-    with the most exchange reactions.
+    Uses a simple heuristic where the external compartment should be the
+    one with the most exchange reactions.
 
-    Arguments
-    ---------
+    Parameters
+    ----------
     model : cobra.Model
-        A cobra model.
+        The cobra model whose external compartments are to be identified.
 
     Returns
     -------
     str
         The putative external compartment.
+
+    Raises
+    ------
+    RuntimeError
+        If several compartments are similar and thus difficult to identify,
+        or, recognized names usually used for external compartment are
+        absent.
+
     """
     if model.boundary:
         counts = pd.Series(tuple(r.compartments)[0] for r in model.boundary)
@@ -52,23 +63,22 @@ def find_external_compartment(model):
 
     if matches.sum() == 1:
         compartment = matches.index[matches][0]
-        LOGGER.info(
-            "Compartment `%s` sounds like an external compartment. "
-            "Using this one without counting boundary reactions" % compartment
+        logger.info(
+            f"Compartment `{compartment}` sounds like an external compartment. "
+            "Using this one without counting boundary reactions."
         )
         return compartment
     elif most is not None and matches.sum() > 1 and matches[most].sum() == 1:
         compartment = most[matches[most]][0]
-        LOGGER.warning(
+        logger.warning(
             "There are several compartments that look like an "
-            "external compartment but `%s` has the most boundary "
-            "reactions, so using that as the external "
-            "compartment." % compartment
+            f"external compartment but `{compartment}` has the most boundary "
+            "reactions, so using that as the external compartment."
         )
         return compartment
     elif matches.sum() > 1:
         raise RuntimeError(
-            "There are several compartments (%s) that look "
+            "There are several compartments that look "
             "like external compartments but we can't tell "
             "which one to use. Consider renaming your "
             "compartments please."
@@ -76,9 +86,9 @@ def find_external_compartment(model):
 
     if most is not None:
         return most[0]
-        LOGGER.warning(
-            "Could not identify an external compartment by name and"
-            " choosing one with the most boundary reactions. That "
+        logger.warning(
+            "Could not identify an external compartment by name and "
+            "choosing one with the most boundary reactions. That "
             "might be complete nonsense or change suddenly. "
             "Consider renaming your compartments using "
             "`Model.compartments` to fix this."
@@ -92,24 +102,26 @@ def find_external_compartment(model):
     )
 
 
-def is_boundary_type(reaction, boundary_type, external_compartment):
+def is_boundary_type(
+    reaction: "Reaction", boundary_type: str, external_compartment: str
+) -> bool:
     """Check whether a reaction is an exchange reaction.
 
-    Arguments
-    ---------
+    Parameters
+    ----------
     reaction : cobra.Reaction
         The reaction to check.
-    boundary_type : str
-        What boundary type to check for. Must be one of
-        "exchange", "demand", or "sink".
+    boundary_type : {"exchange", "demand", "sink"}
+        Boundary type to check for.
     external_compartment : str
-        The id for the external compartment.
+        The ID for the external compartment.
 
     Returns
     -------
-    boolean
+    bool
         Whether the reaction looks like the requested type. Might be based
         on a heuristic.
+
     """
     # Check if the reaction has an annotation. Annotations dominate everything.
     sbo_term = reaction.annotation.get("sbo", "")
@@ -142,29 +154,31 @@ def is_boundary_type(reaction, boundary_type, external_compartment):
     )
 
 
-def find_boundary_types(model, boundary_type, external_compartment=None):
+def find_boundary_types(
+    model: "Model", boundary_type: str, external_compartment: Optional[str] = None
+) -> List["Reaction"]:
     """Find specific boundary reactions.
 
-    Arguments
-    ---------
+    Parameters
+    ----------
     model : cobra.Model
-        A cobra model.
-    boundary_type : str
-        What boundary type to check for. Must be one of
-        "exchange", "demand", or "sink".
-    external_compartment : str or None
-        The id for the external compartment. If None it will be detected
-        automatically.
+        The cobra model whose boundary reactions are to be found.
+    boundary_type : {"exchange", "demand", "sink"}
+        Boundary type to check for.
+    external_compartment : str, optional
+        The ID for the external compartment. If None, it will be detected
+        automatically (default None).
 
     Returns
     -------
-    list of cobra.reaction
+    list of cobra.Reaction or an empty list
         A list of likely boundary reactions of a user defined type.
+
     """
     if not model.boundary:
-        LOGGER.warning(
+        logger.warning(
             "There are no boundary reactions in this model. "
-            "Therefore specific types of boundary reactions such "
+            "Therefore, specific types of boundary reactions such "
             "as 'exchanges', 'demands' or 'sinks' cannot be "
             "identified."
         )
