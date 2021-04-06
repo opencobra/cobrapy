eb317ef5eb24f0d0b4e7867852418d01f78a6405
Synchon Mandal <synchonmandal@gmail.com>
2021-04-06 23:01:31 +0530

    refactor: use relative imports for medium/__init__.py

--- src/cobra/medium/__init__.py
+++ src/cobra/medium/__init__.py
@@ -1,11 +1,7 @@
-# -*- coding: utf-8 -*-
-
-"""Imports for the media module."""
-
-from cobra.medium.boundary_types import (
+from .boundary_types import (
     find_boundary_types,
     find_external_compartment,
     is_boundary_type,
     sbo_terms,
 )
-from cobra.medium.minimal_medium import minimal_medium
+from .minimal_medium import minimal_medium
