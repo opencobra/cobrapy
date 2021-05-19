186723b0d9ad00c805a304c4138d114bd44fe056
Synchon Mandal <synchonmandal@gmail.com>
2021-05-19 16:32:36 +0530

    refactor: add typing annotations and upgrade code style for test_fastcc.py

--- src/cobra/test/test_flux_analysis/test_fastcc.py
+++ src/cobra/test/test_flux_analysis/test_fastcc.py
@@ -1,20 +1,14 @@
-# -*- coding: utf-8 -*-
-
 """Test functionalities of FASTCC."""
-
-
-from __future__ import absolute_import
 
 import pytest
 
-import cobra
+from cobra import Model, Reaction
 from cobra.flux_analysis import fastcc, flux_variability_analysis
 
 
 @pytest.fixture(scope="module")
-def figure1_model():
-    """
-    Generate a toy model as described in [1]_ figure 1.
+def figure1_model() -> Model:
+    """Generate a toy model as described in [1]_ figure 1.
 
     References
     ----------
@@ -24,13 +18,13 @@ def figure1_model():
            PLoS Comput Biol 10(1): e1003424. doi:10.1371/journal.pcbi.1003424
 
     """
-    test_model = cobra.Model("figure 1")
-    v1 = cobra.Reaction("v1")
-    v2 = cobra.Reaction("v2")
-    v3 = cobra.Reaction("v3")
-    v4 = cobra.Reaction("v4")
-    v5 = cobra.Reaction("v5")
-    v6 = cobra.Reaction("v6")
+    test_model = Model("figure 1")
+    v1 = Reaction("v1")
+    v2 = Reaction("v2")
+    v3 = Reaction("v3")
+    v4 = Reaction("v4")
+    v5 = Reaction("v5")
+    v6 = Reaction("v6")
 
     test_model.add_reactions([v1, v2, v3, v4, v5, v6])
 
@@ -53,19 +47,18 @@ def figure1_model():
 
 
 @pytest.fixture(scope="module")
-def opposing_model():
-    """
-    Generate a toy model with opposing reversible reactions.
+def opposing_model() -> Model:
+    """Generate a toy model with opposing reversible reactions.
 
     This toy model ensures that two opposing reversible reactions do not
     appear as blocked.
 
     """
-    test_model = cobra.Model("opposing")
-    v1 = cobra.Reaction("v1")
-    v2 = cobra.Reaction("v2")
-    v3 = cobra.Reaction("v3")
-    v4 = cobra.Reaction("v4")
+    test_model = Model("opposing")
+    v1 = Reaction("v1")
+    v2 = Reaction("v2")
+    v3 = Reaction("v3")
+    v4 = Reaction("v4")
 
     test_model.add_reactions([v1, v2, v3, v4])
 
@@ -83,34 +76,32 @@ def opposing_model():
     return test_model
 
 
-def test_fastcc_benchmark(model, benchmark, all_solvers):
+def test_fastcc_benchmark(model: Model, benchmark, all_solvers) -> None:
     """Benchmark fastcc."""
     model.solver = all_solvers
     benchmark(fastcc, model)
 
 
-def test_figure1(figure1_model, all_solvers):
-    """Test fastcc."""
+def test_figure1(figure1_model: Model, all_solvers) -> None:
+    """Test FASTCC."""
     figure1_model.solver = all_solvers
     consistent_model = fastcc(figure1_model)
     expected_reactions = {"v1", "v3", "v4", "v5", "v6"}
     assert expected_reactions == {rxn.id for rxn in consistent_model.reactions}
 
 
-def test_opposing(opposing_model, all_solvers):
-    """Test fastcc."""
+def test_opposing(opposing_model: Model, all_solvers) -> None:
+    """Test FASTCC."""
     opposing_model.solver = all_solvers
     consistent_model = fastcc(opposing_model)
     expected_reactions = {"v1", "v2", "v3", "v4"}
     assert expected_reactions == {rxn.id for rxn in consistent_model.reactions}
 
 
-def test_fastcc_against_fva_nonblocked_rxns(model, all_solvers):
+def test_fastcc_against_fva_nonblocked_rxns(model: Model, all_solvers) -> None:
     """Test non-blocked reactions obtained by FASTCC against FVA."""
     model.solver = all_solvers
-
     fastcc_consistent_model = fastcc(model)
-
     fva = flux_variability_analysis(model, fraction_of_optimum=0.0)
     nonblocked_rxns_fva = fva.index[
         (fva.minimum.abs() > model.tolerance) | (fva.maximum.abs() > model.tolerance)
