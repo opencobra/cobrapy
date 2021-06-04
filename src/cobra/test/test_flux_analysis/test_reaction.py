72a61147cf284da12867ec6e9d93b8a917d4f480
Synchon Mandal <synchonmandal@gmail.com>
2021-06-04 19:25:13 +0530

    refactor: add typing annotations and upgrade code style for test_reaction.py

--- src/cobra/test/test_flux_analysis/test_reaction.py
+++ src/cobra/test/test_flux_analysis/test_reaction.py
@@ -1,17 +1,13 @@
-# -*- coding: utf-8 -*-
+"""Test assessing functions in flux_analysis.reaction."""
 
-"""Test _assess functions in reaction.py"""
-
-from __future__ import absolute_import
-
-from cobra.core import Metabolite, Reaction
+from cobra.core import Metabolite, Model, Reaction
 from cobra.flux_analysis.reaction import assess
 
 
-def test_assess(model, all_solvers):
+def test_assess(model: Model) -> None:
     """Test assess functions."""
     with model:
-        assert assess(model, model.reactions.GLCpts, solver=all_solvers) is True
+        assert assess(model, model.reactions.GLCpts) is True
         pyr = model.metabolites.pyr_c
         a = Metabolite("a")
         b = Metabolite("b")
@@ -19,7 +15,7 @@ def test_assess(model, all_solvers):
         pyr_a2b = Reaction("pyr_a2b")
         pyr_a2b.add_metabolites({pyr: -1, a: -1, b: 1})
         model.add_reactions([pyr_a2b])
-        res = assess(model, pyr_a2b, 0.01, solver=all_solvers)
+        res = assess(model, pyr_a2b, 0.01)
         expected = {
             "precursors": {a: {"required": 0.01, "produced": 0.0}},
             "products": {b: {"required": 0.01, "capacity": 0.0}},
