4615b7826cc9b29aa2f91972d86f776dd8d0e60d
Synchon Mandal <synchonmandal@gmail.com>
2021-09-16 17:48:35 +0200

    refactor: move around functions to use proper typing annotation in optgp.py

--- src/cobra/sampling/optgp.py
+++ src/cobra/sampling/optgp.py
@@ -14,59 +14,12 @@ from .hr_sampler import HRSampler, shared_np_array
 
 if TYPE_CHECKING:
     from cobra import Model
-    from cobra.sampling import OptGPSampler
 
 
 __all__ = ("OptGPSampler",)
 
 
 configuration = Configuration()
-
-
-def mp_init(obj: "OptGPSampler") -> None:
-    """Initialize the multiprocessing pool."""
-    global sampler
-    sampler = obj
-
-
-# Unfortunately this has to be outside the class to be usable with
-# multiprocessing :()
-def _sample_chain(args: Tuple[int, int]) -> Tuple[int, "OptGPSampler"]:
-    """Sample a single chain for OptGPSampler.
-
-    `center` and `n_samples` are updated locally and forgotten afterwards.
-
-    """
-    n, idx = args  # has to be this way to work in Python 2.7
-    center = sampler.center
-    np.random.seed((sampler._seed + idx) % np.iinfo(np.int32).max)
-    pi = np.random.randint(sampler.n_warmup)
-
-    prev = sampler.warmup[pi, :]
-    prev = step(sampler, center, prev - center, 0.95)
-
-    n_samples = max(sampler.n_samples, 1)
-    samples = np.zeros((n, center.shape[0]))
-
-    for i in range(1, sampler.thinning * n + 1):
-        pi = np.random.randint(sampler.n_warmup)
-        delta = sampler.warmup[pi, :] - center
-
-        prev = step(sampler, prev, delta)
-
-        if sampler.problem.homogeneous and (
-            n_samples * sampler.thinning % sampler.nproj == 0
-        ):
-            prev = sampler._reproject(prev)
-            center = sampler._reproject(center)
-
-        if i % sampler.thinning == 0:
-            samples[i // sampler.thinning - 1, :] = prev
-
-        center = (n_samples * center) / (n_samples + 1) + prev / (n_samples + 1)
-        n_samples += 1
-
-    return (sampler.retries, samples)
 
 
 class OptGPSampler(HRSampler):
@@ -251,3 +204,49 @@ class OptGPSampler(HRSampler):
         d = dict(self.__dict__)
         del d["model"]
         return d
+
+
+def mp_init(obj: OptGPSampler) -> None:
+    """Initialize the multiprocessing pool."""
+    global sampler
+    sampler = obj
+
+
+# Unfortunately this has to be outside the class to be usable with
+# multiprocessing :()
+def _sample_chain(args: Tuple[int, int]) -> Tuple[int, OptGPSampler]:
+    """Sample a single chain for OptGPSampler.
+
+    `center` and `n_samples` are updated locally and forgotten afterwards.
+
+    """
+    n, idx = args  # has to be this way to work in Python 2.7
+    center = sampler.center
+    np.random.seed((sampler._seed + idx) % np.iinfo(np.int32).max)
+    pi = np.random.randint(sampler.n_warmup)
+
+    prev = sampler.warmup[pi, :]
+    prev = step(sampler, center, prev - center, 0.95)
+
+    n_samples = max(sampler.n_samples, 1)
+    samples = np.zeros((n, center.shape[0]))
+
+    for i in range(1, sampler.thinning * n + 1):
+        pi = np.random.randint(sampler.n_warmup)
+        delta = sampler.warmup[pi, :] - center
+
+        prev = step(sampler, prev, delta)
+
+        if sampler.problem.homogeneous and (
+            n_samples * sampler.thinning % sampler.nproj == 0
+        ):
+            prev = sampler._reproject(prev)
+            center = sampler._reproject(center)
+
+        if i % sampler.thinning == 0:
+            samples[i // sampler.thinning - 1, :] = prev
+
+        center = (n_samples * center) / (n_samples + 1) + prev / (n_samples + 1)
+        n_samples += 1
+
+    return (sampler.retries, samples)
