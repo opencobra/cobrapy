ec8d9ec6c339f6caf370c35678510c509af4491f
uri.akavia <uri.akavia@mcgill.ca>
2022-01-10 19:27:11 -0500

    Fixed according to recent review

--- src/cobra/test/test_core/test_gene.py
+++ src/cobra/test/test_core/test_gene.py
@@ -8,17 +8,3 @@ from cobra.core.gene import GPR
 def test_repr_html_(model):
     assert "<table>" in model.genes[0]._repr_html_()
 
-
-def test_gpr():
-    gpr1 = GPR()
-    gpr1.from_string(string_gpr="")
-    assert isinstance(gpr1.body, list)
-    assert len(gpr1.body) == 0
-
-
-def test_gpr_functions():
-    gpr1 = GPR()
-    # Completely empty GPRs will return True
-    assert gpr1.eval()
-    assert gpr1.to_string() == ""
-    gpr2 = gpr1.copy()
