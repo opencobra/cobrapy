25285defecb9ca6d271df8fd18d17f065fdec81f
uri.akavia <uri.akavia@mcgill.ca>
2022-03-06 19:25:03 -0500

    update test_metabolite.py for Python 3.6+ Minor fix for metabolite.py

--- src/cobra/test/test_core/test_metabolite.py
+++ src/cobra/test/test_core/test_metabolite.py
@@ -1,20 +1,24 @@
-# -*- coding: utf-8 -*-
+"""Test functions of metabolite.py."""
 
-"""Test functions of metabolite.py"""
+from typing import TYPE_CHECKING, Tuple
 
 import pytest
 
 from cobra.core import Metabolite
 
 
-def test_metabolite_formula():
+if TYPE_CHECKING:
+    from cobra import Model, Solution
+
+
+def test_metabolite_formula() -> None:
     met = Metabolite("water")
     met.formula = "H2O"
     assert met.elements == {"H": 2, "O": 1}
     assert met.formula_weight == 18.01528
 
 
-def test_formula_element_setting(model):
+def test_formula_element_setting(model: "Model") -> None:
     met = model.metabolites[1]
     orig_formula = str(met.formula)
     orig_elements = dict(met.elements)
@@ -24,20 +28,20 @@ def test_formula_element_setting(model):
     assert met.formula == orig_formula
 
 
-def test_set_id(solved_model):
+def test_set_id(solved_model: Tuple["Solution", "Model"]) -> None:
     solution, model = solved_model
     met = Metabolite("test")
     with pytest.raises(TypeError):
-        setattr(met, "id", 1)
+        met.id = 1
     model.add_metabolites([met])
     with pytest.raises(ValueError):
-        setattr(met, "id", "g6p_c")
+        met.id = "g6p_c"
     met.id = "test2"
     assert "test2" in model.metabolites
     assert "test" not in model.metabolites
 
 
-def test_remove_from_model(solved_model):
+def test_remove_from_model(solved_model: Tuple["Solution", "Model"]) -> None:
     solution, model = solved_model
     met = model.metabolites.get_by_id("g6p_c")
     met.remove_from_model()
@@ -45,5 +49,5 @@ def test_remove_from_model(solved_model):
     assert not (met.id in model.constraints)
 
 
-def test_repr_html_(model):
+def test_repr_html_(model: "Model") -> None:
     assert "<table>" in model.metabolites.h2o_c._repr_html_()
