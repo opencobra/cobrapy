e3e4f143bd4ce7f97f02566de5427d9eb086bb51
uri.akavia <uri.akavia@mcgill.ca>
2022-03-10 15:07:04 -0500

    Update species.py to Python 3.6+

--- src/cobra/core/species.py
+++ src/cobra/core/species.py
@@ -1,14 +1,20 @@
-# -*- coding: utf-8 -*-
+"""Define the Species class, used as a base for Gene and Metabolite."""
 
-from __future__ import absolute_import
 
 from copy import deepcopy
+from typing import TYPE_CHECKING, FrozenSet, Optional, Union
 
-from cobra.core.object import Object
+from ..core.object import Object
+
+
+if TYPE_CHECKING:
+    from .. import Model
 
 
 class Species(Object):
-    """Species is a class for holding information regarding
+    """Species is a base class in Cobrapy.
+
+    Species is a class for holding information regarding
     a chemical Species
 
     Parameters
@@ -19,35 +25,76 @@ class Species(Object):
        A human readable name.
     """
 
-    def __init__(self, id=None, name=None):
+    def __init__(self, id: Optional[str] = None, name: Optional[str] = None) -> None:
+        """Initialize a Species.
+
+        Parameters
+        ----------
+        id : string, optional, default None
+            An identifier for the chemical species
+        name : string, optional, default None
+            A human readable name.
+
+        A species also contains a _model, reference to a cobra.model (initialized as
+        None) and a self._reaction, a set of cobra.reactions (initialized as empty set).
+        """
         Object.__init__(self, id, name)
         self._model = None
         # references to reactions that operate on this species
         self._reaction = set()
 
     @property
-    def reactions(self):
+    def reactions(self) -> FrozenSet:
+        """Return a frozenset of reactions.
+
+        Returns
+        -------
+        FrozenSet
+            A frozenset that includes the reactions of the species.
+        """
         return frozenset(self._reaction)
 
-    def __getstate__(self):
-        """Remove the references to container reactions when serializing to
+    def __getstate__(self) -> dict:
+        """Return the state of the species.
+
+        Remove the references to container reactions when serializing to
         avoid problems associated with recursion.
 
+        Returns
+        -------
+        dict
+            A dictionary describing the state, without the self._reaction to avoid
+            recursion.
         """
         state = Object.__getstate__(self)
         state["_reaction"] = set()
         return state
 
-    def copy(self):
-        """When copying a reaction, it is necessary to deepcopy the
+    def copy(self) -> "Species":
+        """Copy a species.
+
+        When copying a reaction, it is necessary to deepcopy the
         components so the list references aren't carried over.
 
         Additionally, a copy of a reaction is no longer in a cobra.Model.
 
         This should be fixed with self.__deepcopy__ if possible
+
+        Returns
+        -------
+        Species
+            A copy of the species.
         """
         return deepcopy(self)
 
     @property
-    def model(self):
+    def model(self) -> Union[None, "Model"]:
+        """Return the model.
+
+        Returns
+        -------
+        model
+            Returns the cobra model that the species is associated with. None if there
+            is no model associated with this species.
+        """
         return self._model
