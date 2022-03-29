4c9aec2b63570503f13afdb5c0720b073c56d45d
uri.akavia <uri.akavia@mcgill.ca>
2022-03-29 16:09:27 -0400

    inital attempt at loading from cobrapy, via cobrapy_repository.py

---
+++ src/cobra/io/web/cobrapy_repository.py
@@ -0,0 +1,56 @@
+"""Provide functions for loading metabolic models from local package data."""
+
+
+import gzip
+from io import BytesIO
+from typing import List
+
+import importlib_resources
+from .abstract_model_repository import AbstractModelRepository
+
+
+
+class Cobrapy(AbstractModelRepository):
+    """
+    Define a concrete implementation of the cobrapy (local package) repository.
+    """
+
+    def __init__(
+        self,
+        **kwargs,
+    ) -> None:
+        """
+        Initialize a local Cobrapy repository interface.
+
+        Other Parameters
+        ----------------
+        kwargs
+            Passed to the parent constructor in order to enable multiple inheritance.
+
+        """
+        super().__init__(url="file:////", **kwargs)
+
+    def get_sbml(self, model_id: str) -> bytes:
+        """
+        Attempt to open an SBML document from the local repository.
+
+        Parameters
+        ----------
+        model_id : str
+            The identifier of the desired metabolic model. This is typically repository
+            specific.
+
+        Returns
+        -------
+        bytes
+            A gzip-compressed, UTF-8 encoded SBML document.
+
+        Raises
+        ------
+        httpx.HTTPError
+            In case there are any connection problems.
+
+        """
+        cobra_data_dir = importlib_resources.files('cobra.data')
+        model_file = cobra_data_dir.joinpath(f"{model_id}.xml.gz")
+        return model_file.read_bytes()
