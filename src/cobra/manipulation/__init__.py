604110a79b50f3c21349a2fbcd65cefd6ad39571
uri.akavia <uri.akavia@mcgill.ca>
2022-04-09 10:54:11 -0400

    Removed references to delete_model_genes() and undelete_model_genes

--- src/cobra/manipulation/__init__.py
+++ src/cobra/manipulation/__init__.py
@@ -5,7 +5,6 @@ from .delete import (
     prune_unused_metabolites,
     prune_unused_reactions,
     remove_genes,
-    undelete_model_genes,
 )
 from .modify import escape_ID, rename_genes
 from .validate import check_mass_balance, check_metabolite_compartment_formula
