6fd35af152eec18e2dc50e67f2624714beb89267
Synchon Mandal <synchonmandal@gmail.com>
2022-05-01 17:15:00 +0200

    refactor: resolve D100, D103 and add type annotations and docstrings for test_io_order.py

--- tests/test_io/test_io_order.py
+++ tests/test_io/test_io_order.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import
+"""Test functionalities of I/O in an usage order."""
 
 import logging
 from operator import attrgetter
-from os.path import join
+from pathlib import Path
 from random import sample
+from typing import Iterable, List
 
 import pytest
 
@@ -13,32 +12,32 @@ from cobra import DictList, Model
 from cobra import io as cio
 
 
-LOGGER = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope="module")
-def tmp_path_order(tmp_path_factory):
-    return str(tmp_path_factory.mktemp("model_order"))
+def tmp_path_order(tmp_path_factory: Path) -> Path:
+    """Temporary path for I/O order tests."""
+    return tmp_path_factory.mktemp("model_order")
 
 
 @pytest.fixture(scope="module")
-def minimized_shuffle(small_model):
+def minimized_shuffle(small_model: Model) -> Model:
+    """Generate a minimal shuffled model for I/O order tests."""
     model = small_model.copy()
     chosen = sample(list(set(model.reactions) - set(model.exchanges)), 10)
     new = Model("minimized_shuffle")
     new.add_reactions(chosen)
-    LOGGER.debug(
-        "'%s' has %d metabolites, %d reactions, and %d genes.",
-        new.id,
-        new.metabolites,
-        new.reactions,
-        new.genes,
+    logger.debug(
+        f"'{new.id}' has {new.metabolites} metabolites, {new.reactions} reactions, and "
+        f"{new.genes} genes."
     )
     return new
 
 
 @pytest.fixture(scope="module")
-def minimized_sorted(minimized_shuffle):
+def minimized_sorted(minimized_shuffle: Model) -> Model:
+    """Generate a minimal sorted model for I/O order tests."""
     model = minimized_shuffle.copy()
     model.id = "minimized_sorted"
     model.metabolites = DictList(sorted(model.metabolites, key=attrgetter("id")))
@@ -48,7 +47,8 @@ def minimized_sorted(minimized_shuffle):
 
 
 @pytest.fixture(scope="module")
-def minimized_reverse(minimized_shuffle):
+def minimized_reverse(minimized_shuffle: Model) -> Model:
+    """Generate a minimal reversed model for I/O order tests."""
     model = minimized_shuffle.copy()
     model.id = "minimized_reverse"
     model.metabolites = DictList(
@@ -65,16 +65,24 @@ def minimized_reverse(minimized_shuffle):
     scope="module",
     params=["minimized_shuffle", "minimized_reverse", "minimized_sorted"],
 )
-def template(request, minimized_shuffle, minimized_reverse, minimized_sorted):
+def template(
+    request: pytest.FixtureRequest,
+    minimized_shuffle: Model,
+    minimized_reverse: Model,
+    minimized_sorted: Model,
+) -> Model:
+    """Return the cobra Model instances found in the current local symbol table."""
     return locals()[request.param]
 
 
 @pytest.fixture(scope="module", params=["metabolites", "reactions", "genes"])
-def attribute(request):
+def attribute(request: pytest.FixtureRequest) -> str:
+    """Return the parameter passed."""
     return request.param
 
 
-def get_ids(iterable):
+def _get_ids(iterable: Iterable) -> List[str]:
+    """Get IDs for elements in `iterable`."""
     return [x.id for x in iterable]
 
 
@@ -86,14 +94,39 @@ def get_ids(iterable):
         ("load_yaml_model", "save_yaml_model", ".yml"),
     ],
 )
-def test_io_order(attribute, read, write, ext, template, tmp_path_order):
+def test_io_order(
+    attribute: str,
+    read: str,
+    write: str,
+    ext: str,
+    template: Model,
+    tmp_path_order: Path,
+) -> None:
+    """Test loading and saving of models in order.
+
+    Parameters
+    ----------
+    attribute : str
+        The attribute of cobra Model to access.
+    read : str
+        The function name for loading model, defined as string.
+    write : str
+        The function name for saving model, defined as string.
+    ext : str
+        The extension of the file format for loading and saving model.
+    template : cobra.Model
+        The cobra Model instance to load and save.
+    tmp_path_order : pathlib.Path
+        The folder path for storing I/O order test files.
+
+    """
     read = getattr(cio, read)
     write = getattr(cio, write)
-    filename = join(tmp_path_order, "template" + ext)
-    write(template, filename)
-    model = read(filename)
-    model_elements = get_ids(getattr(model, attribute))
-    template_elements = get_ids(getattr(template, attribute))
+    file_path = tmp_path_order / f"template{ext}"
+    write(template, str(file_path.resolve()))
+    model = read(str(file_path.resolve()))
+    model_elements = _get_ids(getattr(model, attribute))
+    template_elements = _get_ids(getattr(template, attribute))
     assert len(model_elements) == len(template_elements)
     assert set(model_elements) == set(template_elements)
     assert model_elements == template_elements
