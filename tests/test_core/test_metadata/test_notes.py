b57b5a1a1d8ed4e7c01a0ec342a4c027285aa71b
uri.akavia <uri.akavia@mcgill.ca>
2022-06-11 11:56:48 -0400

    unified test_io/test_notes.py and test_metadata/test_notes.py

--- tests/test_core/test_metadata/test_notes.py
+++ tests/test_core/test_metadata/test_notes.py
@@ -1,9 +1,47 @@
-import os
+from pathlib import Path
 
 import pytest
 
-from cobra.core.metadata import Notes
+from cobra import Metabolite, Model, Reaction
+from cobra.core import Notes
 from cobra.io import load_json_model, read_sbml_model, save_json_model, write_sbml_model
+
+
+def test_notes_io(tmp_path: Path) -> None:
+    """Test if model notes are written and read from/to SBML.
+
+    Parameters
+    ----------
+    tmp_path : pathlib.Path
+        The path to the temporary test assets store.
+
+    """
+    path_to_file = tmp_path / "model_notes.xml"
+
+    # making a minimal cobra model to test notes
+    model = Model("e_coli_core")
+    model.notes = Notes().notes_from_dict({"Remark": "...Model Notes..."})
+    met = Metabolite("pyr_c", compartment="c")
+    model.add_metabolites([met])
+    met.notes = Notes().notes_from_dict({"Remark": "Note with \n newline"})
+    rxn = Reaction("R_ATPM")
+    model.add_reactions([rxn])
+    rxn.notes = Notes().notes_from_dict({"Remark": "What about me?"})
+    model.objective_direction = "max"
+    model.objective = rxn
+    write_sbml_model(model, str(path_to_file.resolve()))
+
+    # reading the model back
+    model_after_reading = read_sbml_model(str(path_to_file.resolve()))
+    met_after_reading = model_after_reading.metabolites.get_by_id("pyr_c")
+    reaction_after_reading = model_after_reading.reactions.get_by_id("R_ATPM")
+
+    # checking if notes are written to model
+    assert model_after_reading.notes["Remark"] == "...Model Notes..."
+
+    # checking notes for metabolite and reaction
+    assert met_after_reading.notes["Remark"] == "Note with \n newline"
+    assert reaction_after_reading.notes["Remark"] == "What about me?"
 
 
 NEW_VALUE1 = "New Value 1"
@@ -51,8 +89,8 @@ modified_notes_str = (
 
 def test_notes(data_directory, tmp_path):
     """Test creating notes from SBML to cobra model."""
-    model_path = os.path.join(data_directory, "e_coli_core_for_annotation.xml")
-    assert os.path.exists(model_path)
+    model_path = data_directory / "e_coli_core_for_annotation.xml"
+    assert model_path.exists()
     model = read_sbml_model(model_path)
     rx1 = model.reactions[0]
     # making notes object to test equality check of
@@ -87,7 +125,7 @@ def test_notes(data_directory, tmp_path):
     assert rx1.notes["Key3"] == NEW_VALUE3
 
     # writing and reading back the model
-    path_to_file = os.path.join(tmp_path, "model_notes.xml")
+    path_to_file = tmp_path / "model_notes.xml"
     write_sbml_model(model, path_to_file)
 
     model_after_reading = read_sbml_model(path_to_file)
@@ -102,16 +140,14 @@ def test_notes(data_directory, tmp_path):
 
 def test_reading_writing_notes(data_directory, tmp_path):
     # reading model with notes
-    model = read_sbml_model(
-        os.path.join(data_directory, "e_coli_core_for_annotation.xml")
-    )
+    model = read_sbml_model(data_directory / "e_coli_core_for_annotation.xml")
 
     # checking notes data
     rx1 = model.reactions[0]
     assert rx1.notes.notes_xhtml == incoming_notes_str
 
     # reading and writing in json format
-    path_to_json = os.path.join(str(tmp_path), "json_notes.json")
+    path_to_json = tmp_path / "json_notes.json"
     save_json_model(model, path_to_json)
     model_from_json = load_json_model(path_to_json)
     rx1_from_json = model_from_json.reactions[0]
