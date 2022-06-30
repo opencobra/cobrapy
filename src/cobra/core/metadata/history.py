5313d3a36742afe2a6dfd20a2998fb9d1ff462cc
uri.akavia <uri.akavia@mcgill.ca>
2022-06-30 19:39:47 -0400

    history.py is documented

--- src/cobra/core/metadata/history.py
+++ src/cobra/core/metadata/history.py
@@ -57,31 +57,87 @@ class History:
 
     @property
     def creators(self) -> List["Creator"]:
+        """Get creators for History.
+
+        Returns
+        -------
+        list
+            A list of Creator objects.
+        """
         return self._creators
 
     @creators.setter
-    def creators(self, values: Iterable["Creator"]) -> None:
+    def creators(self, values: Iterable[Union[Dict, "Creator"]]) -> None:
+        """Set creators for History.
+
+        Parameters
+        ----------
+        values: iterable
+            An iterable of dictionaries and/or Creator objects.
+        """
         self._creators = [Creator.from_data(v) for v in values]
 
     @property
     def created_date(self) -> "HistoryDatetime":
+        """Get created date for History.
+
+        Returns
+        -------
+        HistoryDatetime
+        """
         return self._created_date
 
     @created_date.setter
     def created_date(self, date: Union[str, "HistoryDateTime"]) -> None:
+        """Set created date for History.
+
+        Parameters
+        ----------
+        date: str or HistoryDateTime
+        """
         self._created_date = HistoryDatetime(date)
 
     @property
     def modified_dates(self) -> List:
+        """Get modified dates.
+
+        Returns
+        -------
+        list
+            List of HistoryDateTimes when this annotation was modified, if any exist.
+            List can be empty.
+        """
         return self._modified_dates
 
     @modified_dates.setter
     def modified_dates(self, dates: Iterable[Union[str, "HistoryDateTime"]]) -> None:
+        """Set modified dates.
+
+        Parameters
+        -------
+        list
+            List of HistoryDateTimes or dictionaries when this annotation was modified.
+        """
         self._modified_dates = [HistoryDatetime(d) for d in dates]
 
     @staticmethod
     def from_data(data: Union[Dict, "History"]) -> "History":
-        """Parse history from data."""
+        """Parse history from data.
+
+        Parameters
+        ----------
+        data: dict or History
+            Dict will be parsed to History object.
+
+        Returns
+        -------
+        History
+
+        Raises
+        ------
+        TypeError
+            If data is neither dict, History or None.
+        """
         if data is None:
             return History()
         elif isinstance(data, History):
@@ -92,7 +148,7 @@ class History:
             raise TypeError(f"Unsupported type for History: '{data}'")
 
     def is_empty(self) -> bool:
-        """Checks if history is empty.
+        """Check if history is empty.
 
         Returns
         -------
@@ -108,7 +164,7 @@ class History:
         return True
 
     def __eq__(self, history: "History") -> bool:
-        """Checking equality of two history objects.
+        """Check equality of two history objects.
 
         A history is equal if all attributes are equal.
 
@@ -137,7 +193,7 @@ class History:
         return True
 
     def to_dict(self) -> Dict:
-        """Returns dictionary representation.
+        """Return dictionary representation of History.
 
         Returns
         -------
@@ -146,6 +202,7 @@ class History:
         "creators": list[dict]
         "created_date": str
         "modified_dates": list[str]
+        }
         """
         return {
             "creators": [c.to_dict() for c in self.creators],
@@ -154,11 +211,27 @@ class History:
         }
 
     def __str__(self) -> str:
+        """Return a string representation.
+
+        Returns
+        -------
+        str
+            History in a flattened dictionary.
+        """
         return str(self.to_dict())
 
     def __repr__(self):
-        return f"<{self.__class__.__name__} {self.creators}" \
-               f" {self.created_date.datetime}>"
+        """Return a string with module and class name.
+
+        Returns
+        -------
+        str
+            History in a string, with module and class name.
+        """
+        return (
+            f"{self.__class__.__module__}.{self.__class__.__qualname__}"
+            f"({self.creators}, {self.created_date.datetime}, {self.modified_dates})"
+        )
 
 
 class Creator:
@@ -174,19 +247,42 @@ class Creator:
 
     def __init__(
         self,
-        given_name: str = None,
-        family_name: str = None,
-        email: str = None,
-        organisation: str = None,
+        given_name: Optional[str] = None,
+        family_name: Optional[str] = None,
+        email: Optional[str] = None,
+        organisation: Optional[str] = None,
     ):
-        self.given_name = given_name  # type: str
-        self.family_name = family_name  # type: str
-        self.email = email  # type: str
-        self.organisation = organisation  # type: str
+        """Initialize Creator class.
+
+        Parameters
+        ----------
+        given_name: str
+            Optional. Default None.
+        family_name: str
+            Optional. Default None.
+        email: str
+            Optional. Default None.
+        organisation: str
+            Optional. Default None.
+        """
+        self.given_name: str = given_name
+        self.family_name: str = family_name
+        self.email: str = email
+        self.organisation: str = organisation
 
     @staticmethod
     def from_data(data: Union[Dict, "Creator"]) -> "Creator":
-        """Parse creator from data."""
+        """Parse creator from data.
+
+        Parameters
+        ----------
+        data: dict or Creator
+            Dictionary will be converted to Creator class.
+
+        Returns
+        -------
+        Creator - the creator in the Creator class
+        """
         if not data:
             return Creator()
         elif isinstance(data, Creator):
@@ -197,21 +293,41 @@ class Creator:
             raise TypeError(f"Invalid format for Creator: {data}")
 
     def __eq__(self, creator_obj: "Creator") -> bool:
-        """Compare creator objects for equality
+        """Compare creator objects for equality.
 
         Two creators are equal if all fields are equal.
+
+        Parameters
+        ----------
+        creator_obj: Creator
+            Creator to compare to.
+
+        Returns
+        -------
+        bool
+            True if all fields are equal, false otherwise.
         """
-        if (
-            self.given_name != creator_obj.given_name
-            or self.family_name != creator_obj.family_name
-            or self.email != creator_obj.email
-            or self.organisation != creator_obj.organisation
-        ):
-            return False
+        for field_name in {"given_name", "family_name", "email", "organisation"}:
+            if self.__getattribute__(field_name) != creator_obj.__getattribute__(
+                field_name
+            ):
+                return False
 
         return True
 
-    def to_dict(self):
+    def to_dict(self) -> Dict:
+        """Convert Creator to dictionary.
+
+        Returns
+        -------
+        dict in this format
+        {
+            "given_name": str,
+            "family_name": str,
+            "email": str,
+            "organisation": str,
+        }
+        """
         return {
             "given_name": self.given_name,
             "family_name": self.family_name,
@@ -220,10 +336,27 @@ class Creator:
         }
 
     def __str__(self) -> str:
+        """Return string representation of Creator.
+
+        Returns
+        -------
+        str
+            String version of flattened dictionary.
+        """
         return str(self.to_dict())
 
     def __repr__(self):
-        return f"<{self.__class__.__name__} {self.given_name} {self.family_name}>"
+        """Return the Creator with module, class, and internal fields.
+
+        Returns
+        -------
+        str
+        """
+        return (
+            f"{self.__class__.__module__}.{self.__class__.__qualname__} "
+            f"({self.given_name} {self.family_name} {self.email} "
+            f"{self.organisation})"
+        )
 
 
 class HistoryDatetime:
@@ -239,18 +372,64 @@ class HistoryDatetime:
     """
 
     def __init__(self, history_datetime: str = None):
+        """Initialize the Datetime class.
+
+        Parameter
+        ---------
+        datetime: str
+            Date in the form of a string.
+            This datetimes must be of the form: %Y-%m-%dT%H:%M:%S%z
+
+        """
         self._datetime: Optional[str] = None
         self.datetime = history_datetime
 
     @property
     def datetime(self) -> str:
+        """Get datetime.
+
+        Returns
+        -------
+        str
+        """
         return self._datetime
 
     @datetime.setter
     def datetime(self, value: str) -> None:
+        """Set datetime.
+
+        Parameters
+        ----------
+        value: str or datetime
+            Can be str or dateitme, and is parsed to make sure it is the right type and
+            format.
+
+        See Also
+        --------
+        self.parse_datetime()
+        """
         self._datetime = self.parse_datetime(value)
 
-    def parse_datetime(self, value: str) -> Optional[str]:
+    def parse_datetime(self, value: str) -> Optional[datetime]:
+        """Parse datetime into str.
+
+        Parameters
+        ----------
+        value: HistoryDatetime or str or datetime
+            Optional. If None, the function will return None.
+            HistoryDateTime and str are converted to datetime.
+            If given datetime, the format will be validated.
+
+        Returns
+        -------
+        datetime: optional
+            Returns None if given None.
+
+        Raises
+        ------
+        TypeError
+            If value is not None, or an instance of HistoryDateTime, str, datetime.
+        """
         if value is None:
             return None
         if isinstance(value, HistoryDatetime):
@@ -315,6 +494,19 @@ class HistoryDatetime:
                     raise ValueError(str(e))
 
     def __eq__(self, history_datetime: "HistoryDatetime") -> bool:
+        """Check equality of two HistoryDateTime entries.
+
+        Parameters
+        ----------
+        history_datetime: HistoryDatetime
+
+        Returns
+        -------
+        bool
+            True if the datetime of both entries is identical, false otherwise.
+        """
+        if not isinstance(history_datetime, HistoryDatetime):
+            return False
         return self.datetime == history_datetime.datetime
 
     def __str__(self) -> str:
@@ -328,5 +520,7 @@ class HistoryDatetime:
 
     def __repr__(self) -> str:
         """Return the HistoryDateTime with module, class, and date."""
-        return f"<{self.__class__.__module__}.{self.__class__.__qualname__}" \
-               f"({self.datetime})>"
+        return (
+            f"<{self.__class__.__module__}.{self.__class__.__qualname__}"
+            f"({self.datetime})>"
+        )
