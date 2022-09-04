4db65b7495950e9073d0edcc601272de533a18c3
Synchon Mandal <synchonmandal@gmail.com>
2022-09-04 13:17:11 +0200

    chore: add docstrings and type annotations in test_gene.py

--- tests/test_core/test_gene.py
+++ tests/test_core/test_gene.py
@@ -1,5 +1,16 @@
-"""Test functions of gene.py"""
+"""Test functions of gene.py ."""
 
 
-def test_repr_html_(model) -> None:
+from cobra.core import Model
+
+
+def test_repr_html_(model: Model) -> None:
+    """Test HTML represenation is correct for a gene.
+
+    Parameters
+    ----------
+    model : cobra.Model
+        The textbook model.
+
+    """
     assert "<table>" in model.genes[0]._repr_html_()
