55ea5e5769b89224d1e0bffe05f095add8ce85ef
Christian Diener <ch.diener@gmail.com>
2024-09-02 13:53:00 +0200

    add tests

--- tests/test_medium/test_boundary_types.py
+++ tests/test_medium/test_boundary_types.py
@@ -8,6 +8,7 @@ from cobra.medium import (
     find_external_compartment,
     is_boundary_type,
 )
+import logging
 
 
 def test_find_external_compartment_single(model: Model) -> None:
@@ -38,6 +39,17 @@ def test_find_external_compartment_multi(model: Model) -> None:
     # Now fails because same boundary count
     with pytest.raises(RuntimeError):
         find_external_compartment(model)
+
+
+@pytest.mark.parametrize("compartment", ["C_e", "e0"])
+def test_find_external_popular_reconstructions(model: Model, compartment, caplog) -> None:
+    """Test some additional id formats."""
+    for ex in model.exchanges:
+        ex.reactants[0].compartment = compartment
+    with caplog.at_level(logging.WARNING):
+        external = find_external_compartment(model)
+    assert external == compartment
+    assert "complete nonsense" not in caplog.text
 
 
 def test_no_names_or_boundary_reactions(empty_model: Model) -> None:
