c018f279a786540eee345bb66ed7c74057ebe41a
Joshua Forrest <jedforrest@gmail.com>
2025-04-15 17:09:18 +1000

    filter check_mass_balance zeros with a floating point comparison

--- src/cobra/core/reaction.py
+++ src/cobra/core/reaction.py
@@ -5,7 +5,7 @@ import re
 from collections import defaultdict
 from copy import copy, deepcopy
 from functools import partial
-from math import isinf
+from math import isinf, isclose
 from operator import attrgetter
 from typing import (
     TYPE_CHECKING,
@@ -1451,7 +1451,7 @@ class Reaction(Object):
             for element, amount in metabolite.elements.items():
                 reaction_element_dict[element] += coefficient * amount
         # filter out 0 values
-        return {k: v for k, v in reaction_element_dict.items() if v != 0}
+        return {k: v for k, v in reaction_element_dict.items() if not isclose(v, 0.0, abs_tol=1e-12)}
 
     @property
     def compartments(self) -> Set:
--- tests/test_core/test_core_reaction.py
+++ tests/test_core/test_core_reaction.py
@@ -333,7 +333,10 @@ def test_mass_balance(model: Model) -> None:
     imbalance = reaction.check_mass_balance()
     assert "H" in imbalance
     assert imbalance["H"] == 1
-
+    # Should be balanced even when coefficients are non-integer
+    reaction_scaled = model.reactions.get_by_id("ATPS4r") * 1.3
+    assert len(reaction_scaled.check_mass_balance()) == 0
+    
 
 def test_build_from_string(model: Model) -> None:
     """Test reaction building from string evaluation."""
